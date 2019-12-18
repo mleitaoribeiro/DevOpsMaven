960d65e603d7a2899c6a82b61289516b5b12fa08
alex <1191743@isep.ipp.pt>
2019-12-18 12:17:21 +0000

    adresses #48 test for Imputs Name

--- src/test/java/switch2019/project/model/PersonTest.java
+++ src/test/java/switch2019/project/model/PersonTest.java
@@ -1,7 +1,23 @@
 package switch2019.project.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
+    @Test
+    @DisplayName("Test for validating imput's name")
+    public void validateName(){
+        //Arrange
+        Person A = new Person(null, null);
+
+        //Act
+        A.setName("Alex");
+        String expected = "Alex";
+
+        //Assert
+        assertEquals(expected, A.getName());
+    }
 }
