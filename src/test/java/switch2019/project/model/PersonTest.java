692925e4ed03e0c08592fc8631dda7bdf021e70b
Marta Ribeiro <1191779@isep.ipp.pt>
2019-12-18 14:15:20 +0000

    fixed #41 - corrections to class PersonTest.

--- src/test/java/switch2019/project/model/PersonTest.java
+++ src/test/java/switch2019/project/model/PersonTest.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
+    /**
+     * Validate Input for Name
+     */
+
     @Test
     @DisplayName("Test for validating imput's name")
     public void validateName(){
@@ -20,4 +24,35 @@ class PersonTest {
         //Assert
         assertEquals(expected, A.getName());
     }
+
+    /**
+     *Validate Input for Birthday Date
+     */
+
+
+    /**
+     *Test if two individuals are the same or not
+     */
+
+
+    /**
+     *Test if multiple siblings were added to siblings list
+     */
+
+
+    /**
+     *Validate if a sibling was added to siblings list
+     */
+
+    /**
+     * Test if multiple siblings were removed to siblings list
+     */
+
+    /**
+     * Validate if a sibling was added to siblings list
+     */
+
+    /**
+     * Test if two people have the same siblings
+     */
 }
