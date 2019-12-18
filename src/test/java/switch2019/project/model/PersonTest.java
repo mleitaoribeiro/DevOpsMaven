575a71d9695073b6e76ba48ed417298cdccb5e66
alex <1191743@isep.ipp.pt>
2019-12-18 12:13:27 +0000

    adresses #48 test for Imputs Name

--- src/test/java/switch2019/project/model/PersonTest.java
+++ src/test/java/switch2019/project/model/PersonTest.java
@@ -1,7 +1,28 @@
 package switch2019.project.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+
+    @Test
+    @DisplayName("Validate Input for Name")
+    public void validateName(){
+        //Arrange
+        //Calendar a = new Calendar(1996, 3, 4);
+        Person A = new Person(null, null);
+
+        //Act
+        A.setName("Alex");
+        String expected = "Alex";
+
+        //Assert
+        assertEquals(expected, A.getName());
+    }
+
 
 }
