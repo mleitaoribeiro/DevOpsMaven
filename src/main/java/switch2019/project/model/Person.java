e976944126b862804a53cf97a685d224c297551e
marta <1191780@isep.ipp.pt>
2019-12-18 11:57:18 +0000

    ref #14 fixed #14 Commit of private attributes of Person in Person Class => Added sibling's list as Hashset of type Person

--- src/main/java/switch2019/project/model/Person.java
+++ src/main/java/switch2019/project/model/Person.java
@@ -1,9 +1,11 @@
 package switch2019.project.model;
 
 import java.util.Calendar;
+import java.util.HashSet;
 
 public class Person {
     // Private instance variables
     private String name;
     private Calendar birthDate;
+    private HashSet<Person> siblingList;
 }
