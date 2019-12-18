661cf358b45aee00f337cf8092ff9ba72eab1bc0
Elsa <1191762@isep.ipp.pt>
2019-12-18 15:04:31 +0000

    Fix #7

--- src/main/java/switch2019/project/model/Person.java
+++ src/main/java/switch2019/project/model/Person.java
@@ -2,6 +2,7 @@ package switch2019.project.model;
 
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Objects;
 
 public class Person {
     // Private instance variables
@@ -67,5 +68,21 @@ public class Person {
 
     public HashSet<Person> getSiblingList() {
         return siblingList;
+    }
+    /**
+     * override of equals for Person Instance and @overrode hashcode
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(birthDate, person.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthDate);
     }
 }
