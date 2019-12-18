77b7667be4569002e6a29695d930cffba643c9b0
Elsa <1191762@isep.ipp.pt>
2019-12-18 15:12:41 +0000

    Fix #12

--- src/main/java/switch2019/project/model/Group.java
+++ src/main/java/switch2019/project/model/Group.java
@@ -7,4 +7,22 @@ public class Group {
     Calendar startingDate;
 
 
+    /**
+     * Default Person constructor
+     * @param description
+     * @param year
+     * @param month
+     * @param day
+     */
+
+    public Group(String description, int year, int month, int day){
+        this.description=description;
+        startingDate = Calendar.getInstance();
+        setStartingDate(year, month, day);
+    }
+    
+    public void setStartingDate(int year, int month, int day) {
+        startingDate.set(year, month, day);
+    }
+
 }
