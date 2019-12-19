cb60e2af42eb661772d0854e857f562d0a91d3f0
marta <1191780@isep.ipp.pt>
2019-12-19 10:18:56 +0000

    ref #84 Added a Group List Control Test Class

---
+++ src/test/java/switch2019/project/controllers/GroupListControlControllerTest.java
@@ -0,0 +1,15 @@
+package switch2019.project.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import switch2019.project.model.Group;
+import switch2019.project.model.Person;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class GroupListControlControllerTest {
+
+}
