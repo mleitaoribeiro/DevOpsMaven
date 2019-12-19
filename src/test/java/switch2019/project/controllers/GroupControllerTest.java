49402c53b969c6790a431c485ceb75866f02984f
Gabriel <1191765@isep.ipp.pt>
2019-12-19 17:12:37 +0000

    Fixed #53:  Test - Validate if group was Created (Single group)

--- src/test/java/switch2019/project/controllers/GroupControllerTest.java
+++ src/test/java/switch2019/project/controllers/GroupControllerTest.java
@@ -15,8 +15,25 @@ class GroupControllerTest {
     /**
      * Method to check if a Group exists
      */
+
     @Test
     public void isGroupInList(){
+        // Arrange Groups
+        Group g1 = new Group(null,2019,12,19);
+
+        //Arrange Group List
+        GroupsList gl = new GroupsList();
+
+        //Act
+        gl.addGroupToGroupList(g1);
+        HashSet<Group> expected = new HashSet<>(Arrays.asList(g1));
+
+        //Assert
+        assertEquals(gl.getGroups(), expected);
+    }
+
+    @Test
+    public void areGroupsInList(){
         // Arrange Groups
         Group g1 = new Group(null,2019,12,19);
         Group g2 = new Group(null, 2019,11,19);
