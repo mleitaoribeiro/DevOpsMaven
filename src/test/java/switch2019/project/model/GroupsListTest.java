d09154e9dbbcd7ed8ddc81d86510cc3f19d4734b
alex <1191743@isep.ipp.pt>
2019-12-19 10:23:13 +0000

    adresses #64 first test to see if group was added to the list

--- src/test/java/switch2019/project/model/GroupsListTest.java
+++ src/test/java/switch2019/project/model/GroupsListTest.java
@@ -1,30 +1,38 @@
 package switch2019.project.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupsListTest {
 
+    
+
+
     /**
      * Test if a Group was added to the list
      */
-/*
+
     @Test
     @DisplayName("Test if the group added is in the list")
     public void testGroupIsInList(){
     //Arrange
-    Group A = new Group(null, "18Dez2019");
-    GroupList groupList = new GroupList();
+    Group A = new Group("Switchieees",2019,12,19);
+    HashSet<Group> expected = new HashSet<>(Collections.singleton(A));
+    GroupsList groupList = new GroupsList();
 
     //Act
-    groupList.add(A);
+    groupList.addGroupToGroupList(A);
 
     //Assert
-    assertEquals(A , groupList.getGroups() );
-
+    assertEquals(expected , groupList.getGroups());
     }
-    */
+
 
 
     /**
