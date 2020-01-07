4d5cc4515a7e016d0ad820d04e6f0e91422973c9
jcardoso <1110120@isep.ipp.pt>
2020-01-07 12:43:02 +0000

    ref #129: Class Account - added method Equals - @override

--- src/main/java/switch2019/project/model/Account.java
+++ src/main/java/switch2019/project/model/Account.java
@@ -1,5 +1,7 @@
 package switch2019.project.model;
 
+import java.util.Objects;
+
 public class Account {
 
     private String denomination;
@@ -9,6 +11,15 @@ public class Account {
     public Account (String accountDenomination, String accountDescription) {
         this.denomination = accountDenomination;
         this.description = accountDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account oneAccount = (Account) o;
+        return Objects.equals(this.denomination, oneAccount.denomination)
+                && Objects.equals(this.description, oneAccount.description);
     }
 
 }
