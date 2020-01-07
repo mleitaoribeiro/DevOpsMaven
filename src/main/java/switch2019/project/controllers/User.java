78b968e3654e761ab0200f30e21d47e1b11c3312
alex <1191743@isep.ipp.pt>
2020-01-07 14:54:13 +0000

    adresses #152 #153 #154 #155 commented methods

--- src/main/java/switch2019/project/controllers/User.java
+++ src/main/java/switch2019/project/controllers/User.java
@@ -9,25 +9,52 @@ public class User {
     private String username;
 
     /**
-     * Constructor for User
+     * Default Person constructor
+     *
+     * @param username
      */
 
     public User (String username){
         this.username = username.toUpperCase();
     }
 
+
+    /**
+     * Add a new category to CategoryList
+     *
+     * @param category
+     */
     public void addCategoryToList(Category category){
 
     }
 
+
+    /**
+     * Remove a category from CategoryList
+     *
+     * @param category
+     */
     public void removeCategoryFromList(Category category){
 
     }
+
+
+    /**
+     * Add multiple categories to CategoryList
+     *
+     * @param categoryList
+     */
 
     public void addMultipleCategoriesToList(CategoryList categoryList){
 
     }
 
+
+    /**
+     * Remove multiple categories from CategoryList
+     *
+     * @param categoryList
+     */
     public void removeMultipleCategoriesToList(CategoryList categoryList){
 
     }
