e6992c2da731c4bb501dc6305a67fdf749855fdb
DiDias <1191755@isep.ipp.pt>
2020-01-07 14:17:06 +0000

    ref#132: Class Account - business - create hashcode for accounts

--- src/main/java/switch2019/project/model/Account.java
+++ src/main/java/switch2019/project/model/Account.java
@@ -22,4 +22,8 @@ public class Account {
                 && Objects.equals(this.description, oneAccount.description);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(denomination, description, balance);
+    }
 }
