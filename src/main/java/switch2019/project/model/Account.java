3351d737ff364d782b4069a912d2e894901e957e
jcardoso <1110120@isep.ipp.pt>
2020-01-07 12:38:11 +0000

    ref #124: Class Account - added Account Constructor

--- src/main/java/switch2019/project/model/Account.java
+++ src/main/java/switch2019/project/model/Account.java
@@ -2,4 +2,13 @@ package switch2019.project.model;
 
 public class Account {
 
+    private String denomination;
+    private String description;
+    private double balance;
+
+    public Account (String accountDenomination, String accountDescription) {
+        this.denomination = accountDenomination;
+        this.description = accountDescription;
+    }
+
 }
