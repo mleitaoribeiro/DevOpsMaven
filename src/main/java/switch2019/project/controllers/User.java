d3d7181164422763246d67528493c6a866274570
alex <1191743@isep.ipp.pt>
2020-01-07 14:47:39 +0000

    adresses #158 created class UserTest

--- src/main/java/switch2019/project/controllers/User.java
+++ src/main/java/switch2019/project/controllers/User.java
@@ -2,8 +2,19 @@ package switch2019.project.controllers;
 
 import java.util.HashSet;
 import switch2019.project.model.Category;
+import switch2019.project.model.CategoryList;
 
 public class User {
+
+    private String username;
+
+    /**
+     * Constructor for User
+     */
+
+    public User (String username){
+        this.username = username.toUpperCase();
+    }
 
     public void addCategoryToList(Category category){
 
@@ -13,11 +24,11 @@ public class User {
 
     }
 
-    public void addMultipleCategoriesToList(HashSet <Category> categoryList){
+    public void addMultipleCategoriesToList(CategoryList categoryList){
 
     }
 
-    public void removeMultipleCategoriesToList(HashSet <Category> categoryList){
+    public void removeMultipleCategoriesToList(CategoryList categoryList){
 
     }
 
