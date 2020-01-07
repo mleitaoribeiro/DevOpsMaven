f078ec3c0f65fb05efe82039923052573b1f94ec
marta <1191780@isep.ipp.pt>
2020-01-07 17:52:53 +0000

    ref #150: Class CategoryTest - Test - Validate if two Categories are Equal Or Not.

--- src/test/java/switch2019/project/model/CategoryTest.java
+++ src/test/java/switch2019/project/model/CategoryTest.java
@@ -70,4 +70,60 @@ class CategoryTest {
             assertEquals("The category description is not valid or it's missing. Please try again.", setterEx.getMessage());
         }
     }
+
+    @Test
+    @DisplayName("Verfiy if two categories are the same - ignore case - Main Scenario")
+    void twoCategoriesAreEquals_ignoreCase() {
+        //Arrange
+        Category categoryDescription = new Category("Health");
+        Category otherCategoryDescription = new Category("HEALTH");
+
+        //Act
+        boolean realResult = categoryDescription.equals(otherCategoryDescription);
+
+        //Assert
+        assertEquals(true, realResult);
+    }
+
+    @Test
+    @DisplayName("Verfiy if two categories are the same - ignore special Character - Main Scenario")
+    void twoCategoriesAreEquals_ignoreSpecialCharacter() {
+        //Arrange
+        Category categoryDescription = new Category("saúde");
+        Category otherCategoryDescription = new Category("saude");
+
+        //Act
+        boolean realResult = categoryDescription.equals(otherCategoryDescription);
+
+        //Assert
+        assertEquals(true, realResult);
+    }
+
+    @Test
+    @DisplayName("Verfiy if two categories are the same - numeric characters - Main Scenario")
+    void twoCategoriesAreEquals_numericCategories() {
+        //Arrange
+        Category categoryDescription = new Category("123");
+        Category otherCategoryDescription = new Category("123");
+
+        //Act
+        boolean realResult = categoryDescription.equals(otherCategoryDescription);
+
+        //Assert
+        assertEquals(true, realResult);
+    }
+
+    @Test
+    @DisplayName("Verfiy if two categories are the same - Not the same")
+    void twoCategoriesAreNotTheSame() {
+        //Arrange
+        Category categoryDescription = new Category("Health");
+        Category otherCategoryDescription = new Category("Education");
+
+        //Act
+        boolean realResult = categoryDescription.equals(otherCategoryDescription);
+
+        //Assert
+        assertEquals(false, realResult);
+    }
 }
