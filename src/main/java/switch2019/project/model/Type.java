c82cc4edcb9fd172bf832785bd4b282aebe16720
Kelle <1191782@isep.ipp.pt>
2020-01-07 14:23:48 +0000

    fixes #123

--- src/main/java/switch2019/project/model/Type.java
+++ src/main/java/switch2019/project/model/Type.java
@@ -2,4 +2,13 @@ package switch2019.project.model;
 
 public class Type {
     boolean description;
+
+    public Type(boolean description) {
+        this.description = description;
+    }
+
+    public boolean isDescription() {
+        return description;
+    }
+    
 }
