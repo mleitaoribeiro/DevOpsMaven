ee89489702996ec57a29da0f11d8258408598daf
jcardoso <1110120@isep.ipp.pt>
2020-01-07 15:04:10 +0000

    ref #137 ref #159 ref #160: Class AccountsList- Created tests to verify if an account has been added to the list and Methods getAccountsList & howManyAccounts

--- src/main/java/switch2019/project/model/AccountsList.java
+++ src/main/java/switch2019/project/model/AccountsList.java
@@ -5,7 +5,32 @@ import java.util.HashSet;
 public class AccountsList {
     private HashSet<Account> accounts;
 
-    public AccountsList(){
-        accounts=new HashSet<Account>();
+    public AccountsList() {
+        accounts = new HashSet<Account>();
+
     }
+
+    /**
+     * method to get Accounts inside an AccountsList
+     * @return AccountsList
+     */
+
+    public HashSet<Account> getAccountsList()
+    {
+        return new HashSet<>(this.accounts);
+    }
+
+    /**
+     * Method to check the number of Accounts inside the list.
+     */
+    public int howManyAccounts(){
+        return this.accounts.size();
+    }
+
+    /**
+     * method to add one account to the list
+     */
+    public void addAccountToAccountsList (Account oneAccount) {
+    }
+
 }
--- src/test/java/switch2019/project/model/AccountsListTest.java
+++ src/test/java/switch2019/project/model/AccountsListTest.java
@@ -1,7 +1,49 @@
 package switch2019.project.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountsListTest {
 
+    @Test
+    @DisplayName("Test if the account was added to the list - the number of accounts on the list was increased")
+    public void testIfAccountsListIncreased() {
+        //Arrange
+        Account oneAccount = new Account ("xpto", "one account");
+        Account otherAccount = new Account ("xyz", "other Account");
+        AccountsList accountsList = new AccountsList();
+
+        //Act
+        accountsList.addAccountToAccountsList(oneAccount);
+        accountsList.addAccountToAccountsList(otherAccount);
+        int result = accountsList.howManyAccounts();
+
+        //Assert
+        assertEquals(2,result);
+    }
+
+
+    @Test
+    @DisplayName("Test if the account was added to the list - the account added is not in the list")
+    public void testAccountIsInList_Not() {
+        //Arrange
+        Account oneAccount = new Account("xpto", "cat acccount");
+        Account otherAccount = new Account ("xyz", "general");
+
+        HashSet <Account> expected = new HashSet<>(Collections.singleton(oneAccount));
+
+        AccountsList accountsList = new AccountsList();
+
+        //Act
+        accountsList.addAccountToAccountsList(otherAccount);
+        HashSet <Account> real = accountsList.getAccountsList();
+
+        //Assert
+        assertNotEquals(expected, real);
+    }
 }
