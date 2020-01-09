019713718d5bcc12863ef2987b94aad7bf1726be
marta <1191780@isep.ipp.pt>
2020-01-09 10:22:57 +0000

    Ref #192 Added CategoryList test Class

---
+++ src/test/java/switch2019/project/model/CategoryListTest.java
@@ -0,0 +1,33 @@
+package switch2019.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryListTest {
+
+    @Test
+    void howManyCategories() {
+    }
+
+    @Test
+    void categoryListContains() {
+    }
+
+    @Test
+    void addCategoryToCategoryList() {
+    }
+
+    @Test
+    void removeCategoryFromList() {
+    }
+
+    @Test
+    void addMultipleCategoriesToLisy() {
+
+    }
+
+    @Test
+    void removeMultipleCategoriesFromlist() {
+}
+}
