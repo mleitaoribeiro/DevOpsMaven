778774fcc85e65aaf69f4da1bcd22c00832cffa2
Marta Pinheiro <1191778@isep.ipp.pt>
2020-01-09 09:52:03 +0000

    fix #185

--- src/test/java/switch2019/project/model/LedgerTest.java
+++ src/test/java/switch2019/project/model/LedgerTest.java
@@ -11,47 +11,92 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LedgerTest {
     /**
-     *Validate if a transaction was added to ledger list
+     * Validate if a transaction was added to ledger list
      */
 
     @Test
     @DisplayName("Test for validating add a new transaction")
-    void addTransactionToLedger() {
+    void addTransactionToLedgerOneTransaction() {
         //Arrange
-        Account account1=new Account("mercearia", "mercearia Continente");
-        Account account2= new Account("transporte","transporte Metro");
-        Category category1= new Category("grossery");
-        Type type1= new Type(true);
-        MonetaryValue monetaryValue1= new MonetaryValue(200, Currency.getInstance("EUR"));
-        Transaction transaction= new Transaction(monetaryValue1,"payment",category1,account1, account2,type1);
-       Ledger ledger1= new Ledger();
-
-         //Act
-        ledger1.addTransactionToLedger(transaction);
-
-        //Assert
-        assertEquals(1, ledger1.getLedgerList().size());
-    }
-    @Test
-    @DisplayName("Test for validating for several new transactions")
-    void addTransactionToLedger_several() {
-        //Arrange
-        Account account1=new Account("mercearia", "mercearia Continente");
-        Account account2= new Account("transporte","transporte Metro");
-        Category category1= new Category("grossery");
-        Category category2= new Category("transport");
-        Type type1= new Type(true);
-        MonetaryValue monetaryValue1= new MonetaryValue(200, Currency.getInstance("EUR"));
-        Transaction transaction1= new Transaction(monetaryValue1,"payment",category1,account1, account2,type1);
-        Transaction transaction2= new Transaction(monetaryValue1,"payment",category2,account1, account2,type1);
-        Ledger ledger1= new Ledger();
+        Account account1 = new Account("mercearia", "mercearia Continente");
+        Account account2 = new Account("transporte", "transporte Metro");
+        Category category = new Category("grocery");
+        Type type = new Type(true);
+        MonetaryValue monetaryValue = new MonetaryValue(200, Currency.getInstance("EUR"));
+        Transaction transaction = new Transaction(monetaryValue, "payment", category, account1, account2, type);
+        Ledger ledger = new Ledger();
 
         //Act
-        ledger1.addTransactionToLedger(transaction1);
-        ledger1.addTransactionToLedger(transaction2);
+        ledger.addTransactionToLedger(transaction);
+
+        //Assert
+        assertEquals(1, ledger.getLedgerList().size());
+    }
+
+    @Test
+    @DisplayName("Test for validating for several new transactions")
+    void addTransactionToLedgerTwoTransaction() {
+        //Arrange
+        Account account1 = new Account("mercearia", "mercearia Continente");
+        Account account2 = new Account("transporte", "transporte Metro");
+        Category category1 = new Category("grossery");
+        Category category2 = new Category("transport");
+        Type type = new Type(true);
+        MonetaryValue monetaryValue = new MonetaryValue(200, Currency.getInstance("EUR"));
+        Transaction transaction1 = new Transaction(monetaryValue, "payment", category1, account1, account2, type);
+        Transaction transaction2 = new Transaction(monetaryValue, "payment", category2, account1, account2, type);
+        Ledger ledger = new Ledger();
+
+        //Act
+        ledger.addTransactionToLedger(transaction1);
+        ledger.addTransactionToLedger(transaction2);
 
 
         //Assert
-        assertEquals(2, ledger1.getLedgerList().size());
+        assertEquals(2, ledger.getLedgerList().size());
+    }
+
+    @Test
+    @DisplayName("Test for validating ledger not adding invalid transactions - null category")
+    void addTransactionToLedgerNullTransactionNullCategory() {
+        //Arrange
+        Account account1 = new Account("mercearia", "mercearia Continente");
+        Account account2 = new Account("transporte", "transporte Metro");
+        Category category = new Category("grocery");
+        Type type = new Type(true);
+        MonetaryValue monetaryValue = new MonetaryValue(200, Currency.getInstance("EUR"));
+        Transaction transaction1 = new Transaction(monetaryValue, "payment", category, account1, account2, type);
+        Transaction transaction2 = new Transaction(monetaryValue, "payment", null, account1, account2, type);
+        Ledger ledger = new Ledger();
+
+        //Act
+        ledger.addTransactionToLedger(transaction1);
+        ledger.addTransactionToLedger(transaction2);
+
+
+        //Assert
+        assertEquals(1, ledger.getLedgerList().size());
+    }
+
+    @Test
+    @DisplayName("Test for validating ledger not adding invalid transactions - null category")
+    void addTransactionToLedgerNullTransactionNullType() {
+        //Arrange
+        Account account1 = new Account("mercearia", "mercearia Continente");
+        Account account2 = new Account("transporte", "transporte Metro");
+        Category category = new Category("grocery");
+        Type type = new Type(true);
+        MonetaryValue monetaryValue = new MonetaryValue(200, Currency.getInstance("EUR"));
+        Transaction transaction1 = new Transaction(monetaryValue, "payment", category, account1, account2, type);
+        Transaction transaction2 = new Transaction(monetaryValue, "payment", category, account1, account2, null);
+        Ledger ledger = new Ledger();
+
+        //Act
+        ledger.addTransactionToLedger(transaction1);
+        ledger.addTransactionToLedger(transaction2);
+
+
+        //Assert
+        assertEquals(1, ledger.getLedgerList().size());
     }
 }
