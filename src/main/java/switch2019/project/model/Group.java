e0fb44266c129b0485c37dc0abfce163a083caa5
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-13 14:37:27 +0000

    ref #241 - Deleted non necessary getter methods and rectified values returned in some methods

--- src/main/java/switch2019/project/model/Group.java
+++ src/main/java/switch2019/project/model/Group.java
@@ -57,71 +57,70 @@ public class Group {
     /**
      * Add a new person to a group
      * @param person
+     * @return true if member was added, false if it wasn't
      */
-    public void addMember(Person person){
+    public boolean addMember(Person person){
         if ( this.members.size() != 0 && person != null) {
-            members.add(person);
+            return members.add(person);
         }
         else if (person != null) {
             setAdmin(person);
+            return true;
         }
+        else
+            return false;
     }
 
     /**
      * Remove one member from a group
      * @param memberToRemove
+     * @return true if member was removed, false if it wasn't
      */
-    public void removeMember(Person memberToRemove){
+    public boolean removeMember(Person memberToRemove){
         if (memberToRemove != null) {
             if (admins.contains(memberToRemove) && admins.size() > 1) {
-                admins.remove(memberToRemove);
-                members.remove(memberToRemove);
+                return admins.remove(memberToRemove) && members.remove(memberToRemove);
             }
             else if (!admins.contains(memberToRemove)) {
-                members.remove(memberToRemove);
+               return members.remove(memberToRemove);
             }
+            else
+                return false;
         }
+        else return false;
     }
-
-    /**
-     * Getter function for the group members
-     * @return membersClone;
-     */
-    public HashSet<Person> getMembers () {
-        return new HashSet<>(this.members);
-    }
-
-    /**
-     * Getter function for the group admins
-     * @return adminsClone
-     */
-    public HashSet<Person> getAdmins() { return new HashSet<>(this.admins);}
 
     /**
      * Add account to Group´s Account List
+     * @param account1
+     * @return true if account was added to GroupAccountsList, false if it wasn't
      */
-    public void addAccountToGroupAccountList(Account account1){
-        this.groupAccountsList.addAccountToAccountsList(account1);
+    public boolean addAccountToGroupAccountList(Account account1){
+        return this.groupAccountsList.addAccountToAccountsList(account1);
     }
 
     /**
      * Add multiple members to Group
-     * @param: newMembers;
+     * @param newMembers
+     * @return true if multiple members were added, false if they weren't
      */
-    public void addMultipleMembers(HashSet<Person>newMembers) {
+    public boolean addMultipleMembers(HashSet<Person>newMembers) {
         for (Person member : newMembers){
-            addMember(member);
+            return addMember(member);
         }
+        return false;
     }
 
     /**
      * Remove multiple Members
-     * @param toRemove HashSet of members that are going to be removed.
+     * @param toRemove HashSet of members that are going to be removed
+     * @return true if multiple members were removed, false if they weren't
      */
-    public void removeMultipleMembers(HashSet<Person> toRemove) {
+    public boolean removeMultipleMembers(HashSet<Person> toRemove) {
         for (Person member : toRemove) {
-            removeMember(member);
+            return removeMember(member);
         }
+        return false;
     }
 
     /**
@@ -150,28 +149,33 @@ public class Group {
 
     /**
      * Setter function to promote a person directly to group administrator
-     *@param person
+     * @param person
+     * @return true if person was promoted, false if it wasn't
      */
-    public void setAdmin (Person person) {
+    public boolean setAdmin (Person person) {
         if (!members.contains(person) && (!admins.contains(person) && person != null)){
-            members.add(person);
-            admins.add(person);
+            return members.add(person) && admins.add(person);
         }
+        else
+            return false;
     }
 
     /**
      * Promote member to group admin.
      * @param member
+     * @return true if person was promoted, false if it wasn't
      */
-    public void promoteMemberToAdmin(Person member) {
+    public boolean promoteMemberToAdmin(Person member) {
         if (members.contains(member) && member != null) {
-            admins.add(member);
+            return admins.add(member);
         }
+        else return false;
     }
 
     /**
      * Promote multiple members to group admins
      * @param multipleMembers
+     * @return true if person was promoted, false if it wasn't
      */
     public void promoteMultipleMemberToAdmin (HashSet<Person> multipleMembers) {
         for (Person member:multipleMembers){
