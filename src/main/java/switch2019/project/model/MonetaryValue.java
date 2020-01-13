c4a44ce9022db41cd547c44976d3771524fc6472
Elsa <1191762@isep.ipp.pt>
2020-01-13 15:27:54 +0000

    Merge branch 'master' of https://bitbucket.org/switch2019/switch2019_g2
     Conflicts:
    	src/main/java/switch2019/project/model/Ledger.java
    	src/main/java/switch2019/project/model/Person.java
    	src/test/java/switch2019/project/model/LedgerTest.java

--- src/main/java/switch2019/project/model/MonetaryValue.java
+++ src/main/java/switch2019/project/model/MonetaryValue.java
@@ -4,12 +4,12 @@ import java.util.Currency;
 
 public class MonetaryValue {
     private double amount;
-    private Currency currency; 
+    private Currency currency;
 
     /**
      * Monetary value constructor
      *
-     * @param amount valor
+     * @param amount   valor
      * @param currency tipo de moeda
      */
     public MonetaryValue(double amount, Currency currency) {
@@ -18,11 +18,11 @@ public class MonetaryValue {
     }
 
     /**
-     * Get Amount
+     * Validate PositiveAmount
      *
-     * @return amount
+     * @return true
      */
-    public double getAmount() {
-        return amount;
+    public boolean validateIfAmountIsPositive() {
+        return true;
     }
 }
---
+++ src/test/java/switch2019/project/model/MonetaryValueTest.java
@@ -0,0 +1,35 @@
+package switch2019.project.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonetaryValueTest {
+
+    @Test
+    @DisplayName("Test for validating monetarValue Positive-true")
+    void validateIfAmountIsPositive() {
+        //Arrange
+        MonetaryValue monetaryValue1 = new MonetaryValue(2.00, Currency.getInstance("eur"));
+        //Act
+        boolean validateIfAmountIsPostive = monetaryValue1.validateIfAmountIsPositive();
+
+        //Assert
+        assertTrue(validateIfAmountIsPostive);
+    }
+
+    @Test
+    @DisplayName("Test for validating monetarValue Negative-false")
+    void validateIfAmountIsNegative() {
+        //Arrange
+        MonetaryValue monetaryValue1 = new MonetaryValue(-2.00, Currency.getInstance("eur"));
+        //Act
+        boolean validateIfAmountIsPostive = monetaryValue1.validateIfAmountIsPositive();
+
+        //Assert
+        assertFalse(validateIfAmountIsPostive);
+    }
+}
