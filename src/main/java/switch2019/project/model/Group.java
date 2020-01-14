f614a634dc342b630c6495bee129dcd7748d4739
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-14 09:11:03 +0000

    fix #241

--- src/main/java/switch2019/project/model/Group.java
+++ src/main/java/switch2019/project/model/Group.java
@@ -192,7 +192,7 @@ public class Group {
      * @param account1
      * @return true if account was added to GroupAccountsList, false if it wasn't
      */
-    public boolean addAccountToGroupAccountList(Account account1){
+    public boolean addAccountToGroupAccountsList(Account account1){
         return this.groupAccountsList.addAccountToAccountsList(account1);
     }
 
@@ -204,7 +204,7 @@ public class Group {
      */
     public boolean createGroupAccount (String accountDenomination, String accountDescription) {
         if (accountDenomination != null) {
-            return this.addAccountToGroupAccountList(new Account(accountDenomination, accountDescription));
+            return this.addAccountToGroupAccountsList(new Account(accountDenomination, accountDescription));
         }
         return false;
     }
