ddab600d37a4c7d268b65844ea335f8de28c00ab
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-21 14:05:02 +0000

    fix #348 - changed equals method to see were the bug is.

--- src/main/java/switch2019/project/model/CategoryList.java
+++ src/main/java/switch2019/project/model/CategoryList.java
@@ -21,7 +21,8 @@ public class CategoryList {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
         CategoryList categoriesList = (CategoryList) o;
         return Objects.equals(categories, categoriesList.categories);
     }
