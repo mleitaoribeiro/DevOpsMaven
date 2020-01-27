d476a16ed0f2377b85c2d6bd6641993bef119d05
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-27 10:18:38 +0000

    fix #416 - tests for US008.1, controller classes and methods signature for created tests

---
+++ src/test/java/switch2019/project/controllers/US008_1Test.java
@@ -0,0 +1,7 @@
+package switch2019.project.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class US008_1Test {
+
+}
--- src/test/java/switch2019/project/model/GroupTest.java
+++ src/test/java/switch2019/project/model/GroupTest.java
@@ -1856,6 +1856,139 @@ class GroupTest {
         assertTrue(result);
     }
 
+    /**
+     * Test if a group transaction was created
+     */
+    @Test
+    @DisplayName("Test if a group transaction was created - success case")
+    void createGroupTransactionSuccessCase() {
+        //Arrange
+        Person person1 = new Person("João", LocalDate.of(2000,12,12), new Address("Porto"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person2 = new Person("Francisca", LocalDate.of(2000,12,12), new Address("Lisboa"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person3 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Person person4 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Group group1 = new Group("Test group");
+        group1.addMember(person3);
+        group1.addMember(person4);
+
+        Account from = new Account("Wallet", "General expenses");
+        Account to = new Account("TransportAccount", "Transport expenses");
+
+        if (group1.isGroupAdmin(person3)) {
+            group1.createGroupAccount("Wallet", "General expenses");
+            group1.createGroupAccount("TransportAccount", "Transport expenses");
+        }
+
+        MonetaryValue amount = new MonetaryValue(20, Currency.getInstance("EUR"));
+
+        String description = "payment";
+
+        Category category = new Category("General");
+        person4.createCategoryAndAddToCategoryList("General");
+
+        boolean type = false; //debit
+
+        //Act
+        boolean transactionCreated = false;
+        if (group1.isGroupMember(person4)) {
+            transactionCreated = group1.createGroupTransaction(amount, description, null, category, from, to, type);
+        }
+
+        //Assert
+        assertTrue(transactionCreated);
+    }
+
+    @Test
+    @DisplayName("Test if a group transaction was created - monetary value is negative")
+    void createGroupTransactionAccountNegativeMonetaryValue() {
+        //Arrange
+        Person person1 = new Person("João", LocalDate.of(2000,12,12), new Address("Porto"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person2 = new Person("Francisca", LocalDate.of(2000,12,12), new Address("Lisboa"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person3 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Person person4 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Group group1 = new Group("Test group");
+        group1.addMember(person3);
+        group1.addMember(person4);
+
+        Account from = new Account("Wallet", "General expenses");
+        Account to = new Account("TransportAccount", "Transport expenses");
+
+        if (group1.isGroupAdmin(person3)) {
+            group1.createGroupAccount("Wallet", "General expenses");
+            group1.createGroupAccount("TransportAccount", "Transport expenses");
+        }
+
+        MonetaryValue amountNegative = new MonetaryValue(-50, Currency.getInstance("EUR"));
+        String description1 = "payment";
+
+        Category category = new Category("General");
+        person4.createCategoryAndAddToCategoryList("General");
+
+        boolean type = false; //debit
+
+        //Act
+        try {
+            if (group1.isGroupMember(person4)) {
+                group1.createGroupTransaction(amountNegative, description1, null, category, from, to, type);
+            }
+        }
+
+        //Assert
+        catch (IllegalArgumentException description) {
+            assertEquals("The monetary value can´t be null or negative. Please try again.", description.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Test if a transaction was created - person is not a member of the group")
+    void createGroupTransactionNotMember() {
+        //Arrange
+        Person person1 = new Person("João", LocalDate.of(2000,12,12), new Address("Porto"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person2 = new Person("Francisca", LocalDate.of(2000,12,12), new Address("Lisboa"),
+                new Address("Rua dos Flores", "Porto", "4450-852"));
+        Person person3 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Person person4 = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),
+                new Address ("Rua X", "Porto", "4520-266"), person2, person1);
+        Group group1 = new Group("Test group");
+        group1.addMember(person3);
+
+        Account from = new Account("Wallet", "General expenses");
+        Account to = new Account("TransportAccount", "Transport expenses");
+
+        if (group1.isGroupAdmin(person3)) {
+            group1.createGroupAccount("Wallet", "General expenses");
+            group1.createGroupAccount("TransportAccount", "Transport expenses");
+        }
+
+        MonetaryValue amount = new MonetaryValue(20, Currency.getInstance("EUR"));
+
+        String description = "payment";
+
+        Category category = new Category("General");
+        person4.createCategoryAndAddToCategoryList("General");
+
+        boolean type = false; //debit
+
+        //Act
+        boolean transactionCreated = false;
+        if (group1.isGroupMember(person4)) {
+            transactionCreated = group1.createGroupTransaction(amount, description, null, category, from, to, type);
+        }
+
+        //Assert
+        assertFalse(transactionCreated);
+    }
+
 }
 
 
