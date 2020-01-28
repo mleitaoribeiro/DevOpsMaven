985ef7c0f4a8ee4d753e40ca695114dad870b998
Kelle <1191782@isep.ipp.pt>
2020-01-28 09:41:10 +0000

    fixes #451

--- src/main/java/switch2019/project/controllers/PersonalTransactionsFromPeriodController.java
+++ src/main/java/switch2019/project/controllers/PersonalTransactionsFromPeriodController.java
@@ -1,15 +1,14 @@
 package switch2019.project.controllers;
 
-import switch2019.project.model.Ledger;
 import switch2019.project.model.Person;
 import switch2019.project.model.Transaction;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
 
 public class PersonalTransactionsFromPeriodController {
 
-    public ArrayList<Transaction> returnPersonLedgerFromPeriod(LocalDateTime initialDate, LocalDateTime finalDate, Person personController) {
+    public List<Transaction> returnPersonLedgerFromPeriod(LocalDateTime initialDate, LocalDateTime finalDate, Person personController) {
         return personController.returnPersonLedgerFromPeriod(initialDate, finalDate);
     }
 }
