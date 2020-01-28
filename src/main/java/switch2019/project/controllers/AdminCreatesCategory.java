27f030210a91490188dfb37148226629d71bdbaa
jcardoso <1110120@isep.ipp.pt>
2020-01-28 09:50:03 +0000

    ref #462: Added deleted Method to create Category again due to merge

--- src/main/java/switch2019/project/controllers/AdminCreatesCategory.java
+++ src/main/java/switch2019/project/controllers/AdminCreatesCategory.java
@@ -1,4 +1,18 @@
 package switch2019.project.controllers;
 
+import switch2019.project.model.Group;
+import switch2019.project.model.Person;
+
 public class AdminCreatesCategory {
+
+    public boolean createCategory(String categorDescription, Group oneGroup, Person categoryCreator) {
+
+        //Check if the Category Creator is a member of the group
+        if (oneGroup.isGroupMember(categoryCreator)) {
+            // Add the transaction to the Group Ledger:
+            return oneGroup.createAndAddCategoryToCategoryList(categorDescription);
+        }
+        return false;//If the Category creator isn´t a member of the group the Category is not created
+    }
 }
+
