806962fda6dd154cba0234e41112d540b1fa0608
jcardoso <1110120@isep.ipp.pt>
2020-01-28 10:08:08 +0000

    ref #464: Class Ledger - Created Method to sort Ledger By Descending Transaction Date

--- src/main/java/switch2019/project/controllers/AdminCreatesCategory.java
+++ src/main/java/switch2019/project/controllers/AdminCreatesCategory.java
@@ -5,14 +5,14 @@ import switch2019.project.model.Person;
 
 public class AdminCreatesCategory {
 
-    public boolean createCategory(String categorDescription, Group oneGroup, Person categoryCreator) {
+    public boolean createCategory(String categoryDescription, Group oneGroup, Person categoryCreator) {
 
         //Check if the Category Creator is the group admin
         if (oneGroup.isGroupAdmin(categoryCreator)) {
             // Add the category to the Group Ledger:
-            return oneGroup.createAndAddCategoryToCategoryList(categorDescription);
+            return oneGroup.createAndAddCategoryToCategoryList(categoryDescription);
         }
-        return false;//If the Category creator isn´t a member of the group the Category is not created
+        return false; //If the Category creator isn´t the admin of the group the Category is not created
     }
 }
 
