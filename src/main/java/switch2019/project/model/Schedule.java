d8cc571465544eb64ed2e36109ba7906f2928326
alex <1191743@isep.ipp.pt>
2020-01-28 12:05:58 +0000

    fix #481 created class Schedule

---
+++ src/main/java/switch2019/project/model/Schedule.java
@@ -0,0 +1,12 @@
+package switch2019.project.model;
+
+import java.time.LocalDateTime;
+
+public class Schedule {
+
+    private String description;
+    private LocalDateTime date;
+    private boolean type;
+    private String periodicity;
+
+}
