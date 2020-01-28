fc4277fea4dc121efb6ae7e95c2ed3dd52fe6248
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-28 18:25:43 +0000

    fix #447

--- src/main/java/switch2019/project/controllers/MemberGetsBalanceInDateRangeController.java
+++ src/main/java/switch2019/project/controllers/MemberGetsBalanceInDateRangeController.java
@@ -5,11 +5,12 @@ import switch2019.project.model.*;
 import java.time.LocalDateTime;
 
 public class MemberGetsBalanceInDateRangeController {
+
     /**
      * US018
      * As a group member, i want to to get the balance of group transactions given a specific date range
      */
-    public double getGroupBalanceInDateRange(LocalDateTime initialDate, LocalDateTime finalDate, Group group1, Person member) throws IllegalArgumentException {
+    public double getGroupBalanceInDateRange(LocalDateTime initialDate, LocalDateTime finalDate, Group group1, Person member) {
         if (!group1.isGroupMember(member)) {
             throw new IllegalArgumentException("The group member is not valid.");
         }
