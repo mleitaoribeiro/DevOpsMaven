3f2b37193c1e5e1765fe6f637d13d93522c5de93
alex <1191743@isep.ipp.pt>
2020-01-28 10:40:35 +0000

    ref #449 updated method signatures for controller

--- src/main/java/switch2019/project/controllers/PersonalTransactionsInDateRangeController.java
+++ src/main/java/switch2019/project/controllers/PersonalTransactionsInDateRangeController.java
@@ -6,7 +6,7 @@ import switch2019.project.model.Transaction;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class PersonalTransactionsFromPeriodController {
+public class PersonalTransactionsInDateRangeController {
 
     /**
      * US011: As a user, I want to get my transactions in a specific date range
