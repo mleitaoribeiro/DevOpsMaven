9b14f2b0003b51af1fdb7fd92a0a0dc9afc8ecc9
Gabriel Moço (1191765) <1191765@isep.ipp.pt>
2020-01-28 12:03:25 +0000

    fix #479

--- src/main/java/switch2019/project/model/Group.java
+++ src/main/java/switch2019/project/model/Group.java
@@ -300,8 +300,8 @@ public class Group {
      * @param nameOfcategory
      */
 
-    public boolean removeCategoryFromList(String nameOfcategory) {
-        if (nameOfcategory == null) {
+    public boolean removeCategoryFromList(String nameOfcategory, Person groupAdmin) {
+        if (nameOfcategory == null && this.isGroupAdmin(groupAdmin)) {
             return false;
         }
         return categoryList.removeCategoryFromList(nameOfcategory);
