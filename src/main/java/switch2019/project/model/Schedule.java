e97e2dcc311a7624d76d14a50781094a34a0f6b9
alex <1191743@isep.ipp.pt>
2020-01-29 09:49:35 +0000

    ref #491 created constructor for Schedule

--- src/main/java/switch2019/project/model/Schedule.java
+++ src/main/java/switch2019/project/model/Schedule.java
@@ -1,12 +1,36 @@
 package switch2019.project.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDateTime; 
 
 public class Schedule {
 
     private String description;
     private LocalDateTime date;
-    private boolean type;
+    private Transaction transaction;
     private String periodicity;
+
+    public Schedule(Transaction transaction, String periodicity) {
+        this.transaction = transaction;
+        this.periodicity = periodicity;
+    }
+
+    public Schedule scheduleATransaction(Schedule schedule) {
+        switch (schedule.periodicity) {
+            case "mensal":
+
+                break;
+            case "diário":
+
+                break;
+            case "dias úteis":
+
+                break;
+            case "semanal":
+
+                break;
+
+        }
+        return schedule;
+    }
 
 }
