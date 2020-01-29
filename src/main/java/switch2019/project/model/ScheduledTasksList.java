f404f39e37441305aca3af3395dd0c2ee9f4d645
Marta Pinheiro <1191778@isep.ipp.pt>
2020-01-29 10:49:44 +0000

    fix #498

--- src/main/java/switch2019/project/model/ScheduledTasksList.java
+++ src/main/java/switch2019/project/model/ScheduledTasksList.java
@@ -7,8 +7,12 @@ public class ScheduledTasksList {
 
     private HashSet<Schedule> scheduledTasksList;
 
+    /**
+     * Method to add a new schedule do the scheduledTasksList
+     */
     public boolean addNewSchedule(Person person, String periodicity, MonetaryValue amount, String descripiton, LocalDateTime date,
                                   Category category, Account accountFrom, Account accountTo, boolean type) {
+
         Schedule newSchedule = new Schedule(person, periodicity, amount, descripiton, date, category, accountFrom, accountTo, type);
         return scheduledTasksList.add(newSchedule);
     }
