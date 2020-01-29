40ac14426f5c9c3f709a53f7b18528244a3dbc43
Marta Pinheiro <1191778@isep.ipp.pt>
2020-01-29 12:10:45 +0000

    ref #502 - added mom and dad to Person created

--- src/test/java/switch2019/project/model/PersonTest.java
+++ src/test/java/switch2019/project/model/PersonTest.java
@@ -1678,7 +1678,12 @@ class PersonTest {
     void scheduleNewTransactionDaily() throws InterruptedException {
 
         //Arrange
-        Person person = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person dad = new Person("Carlos", LocalDate.of(1980,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person mom = new Person("Manuela", LocalDate.of(1980,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person person = new Person("Jose", LocalDate.of(1995,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"), mom, dad);
 
         MonetaryValue amount = new MonetaryValue(20, Currency.getInstance("EUR"));
         String description = "payment";
@@ -1708,7 +1713,12 @@ class PersonTest {
     void scheduleNewTransactionMonthly() throws InterruptedException {
 
         //Arrange
-        Person person = new Person("Jose", LocalDate.of(1995,12,13), new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person dad = new Person("Carlos", LocalDate.of(1980,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person mom = new Person("Manuela", LocalDate.of(1980,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"));
+        Person person = new Person("Jose", LocalDate.of(1995,12,13),
+                new Address("Lisboa"),new Address ("Rua X", "Porto", "4520-266"), mom, dad);
 
         MonetaryValue amount = new MonetaryValue(20, Currency.getInstance("EUR"));
         String description = "payment";
