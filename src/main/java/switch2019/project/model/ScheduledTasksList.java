40daa11338d2f477b8a80f1763608c0e0889d594
Marta Ribeiro <1191779@isep.ipp.pt>
2020-01-30 17:23:39 +0000

    fix #552

--- src/main/java/switch2019/project/model/ScheduledTasksList.java
+++ src/main/java/switch2019/project/model/ScheduledTasksList.java
@@ -23,14 +23,16 @@ public class ScheduledTasksList {
                                   Category category, Account accountFrom, Account accountTo, boolean type) {
 
         Schedule newSchedule = new Schedule(person, periodicity, amount, descripiton, date, category, accountFrom, accountTo, type);
-        return scheduledTransactions.add(newSchedule) && scheduledTransactions.contains(newSchedule);
+        scheduledTransactions.add(newSchedule);
+        return scheduledTransactions.contains(newSchedule);
     }
 
     public boolean addNewSchedule(Group group, String periodicity, MonetaryValue amount, String descripiton, LocalDateTime date,
                                   Category category, Account accountFrom, Account accountTo, boolean type) {
 
         Schedule newSchedule = new Schedule(group, periodicity, amount, descripiton, date, category, accountFrom, accountTo, type);
-        return scheduledTransactions.add(newSchedule) && scheduledTransactions.contains(newSchedule);
+        scheduledTransactions.add(newSchedule);
+        return scheduledTransactions.contains(newSchedule);
     }
 
 }
