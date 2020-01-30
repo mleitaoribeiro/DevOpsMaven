a936c9d1901db4c95f7a841a8b914eb781399962
Gabriel Moço (1191765) <1191765@isep.ipp.pt>
2020-01-30 11:59:37 +0000

    ref #540 returnTransactionsFromAllGroupsAPersonIsIn tests on the GroupsListTest class implemented.

--- src/test/java/switch2019/project/controllers/GetTheMovementsOfAnAccountInDateRangeControllerTest.java
+++ src/test/java/switch2019/project/controllers/GetTheMovementsOfAnAccountInDateRangeControllerTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserGetTransactionControllerTest {
+public class GetTheMovementsOfAnAccountInDateRangeControllerTest {
 
     /**
      * US010.1 Como utilizador, quero obter os movimentos de determinada conta num dado período.
@@ -56,7 +56,7 @@ public class UserGetTransactionControllerTest {
         person.createTransaction(monetaryValue2, "payment", LocalDateTime.of(2019, 12, 25, 12, 15), category2, account2, account, type2);
 
         //Act
-        List<Transaction> listOfTransactions = controller.getOneAccountMovementsFromPerson(account, initialDate, finalDate,person);
+        List<Transaction> listOfTransactions = controller.getOneAccountTransactionsFromPerson(account, initialDate, finalDate,person);
 
 
         //Assert
@@ -101,7 +101,7 @@ public class UserGetTransactionControllerTest {
         person.createTransaction(monetaryValue2, "payment", LocalDateTime.of(2019, 12, 25, 12, 15), category2, account2, account5, type2);
 
         //Act
-        List<Transaction> listOfTransactions = controller.getOneAccountMovementsFromPerson(account5, date1, date2,person);
+        List<Transaction> listOfTransactions = controller.getOneAccountTransactionsFromPerson(account5, date1, date2,person);
 
 
         //Assert
