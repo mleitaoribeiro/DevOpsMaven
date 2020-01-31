4e0b617e1e7859f499cea06b02eaa2f3328e8e1c
Gabriel Moço (1191765) <1191765@isep.ipp.pt>
2020-01-31 10:56:43 +0000

    ref #442

--- src/main/java/switch2019/project/controllers/MemberCreatesGroupTransactionController.java
+++ src/main/java/switch2019/project/controllers/MemberCreatesGroupTransactionController.java
@@ -7,9 +7,19 @@ import java.time.LocalDateTime;
 public class MemberCreatesGroupTransactionController {
 
     /**
-     * US008.1
-     * As a group member, i want to create a group transaction by atribuing a value, a description, a category,
-     * a debit account and a credit account
+     * Controller that enables a member to create a Group Transaction
+     *
+     * @param amount
+     * @param description
+     * @param localDate
+     * @param category
+     * @param accountFrom
+     * @param accountTo
+     * @param type
+     * @param group1
+     * @param transactionCreator
+     * @return True .- if transactionCreator is a group member AND the transaction is verified to be inside the groupLedger.
+     *         False .- if transactionCreator is not a group member OR the transaction is not inside the groupLedger.
      */
 
     public boolean memberCreatesAGroupTransaction (MonetaryValue amount, String description, LocalDateTime localDate,
