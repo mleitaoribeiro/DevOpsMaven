af029ee8b96c4d0e5c4fdf15aa8c62d06b35e7fe
Marta Pinheiro <1191778@isep.ipp.pt>
2020-03-04 14:52:57 +0000

    fix #598

--- src/test/java/switch2019/project/model/PersonTest.java
+++ src/test/java/switch2019/project/model/PersonTest.java
@@ -9,6 +9,7 @@ import switch2019.project.model.ledger.Transaction;
 import switch2019.project.model.person.Person;
 import switch2019.project.model.category.Category;
 import switch2019.project.model.valueObject.Address;
+import switch2019.project.repository.CategoryList;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
