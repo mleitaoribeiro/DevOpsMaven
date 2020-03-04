619e30f8ea6d9a37bd02714c9a830b21752ea40d
Gabriel Moço (1191765) <1191765@isep.ipp.pt>
2020-03-04 13:59:30 +0000

    ref #578 User Story 8 Arranged in the Controller Package

---
+++ src/main/java/switch2019/project/controllers/US008PersonCreatesTransactionController.java
@@ -0,0 +1,8 @@
+package switch2019.project.controllers;
+
+/**
+ * Person creates a transaction which is added to his personal Ledger
+ */
+
+public class US008PersonCreatesTransactionController {
+}
