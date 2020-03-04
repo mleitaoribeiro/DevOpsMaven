fb260d0348a134c03e98ae82e6fd738ed82de6da
Marta Pinheiro <1191778@isep.ipp.pt>
2020-03-04 15:21:18 +0000

    ref #598

--- src/test/java/switch2019/project/model/group/GroupTest.java
+++ src/test/java/switch2019/project/model/group/GroupTest.java
@@ -3,7 +3,6 @@ package switch2019.project.model.group;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import switch2019.project.model.account.Account;
-import switch2019.project.model.group.Group;
 import switch2019.project.model.ledger.Transaction;
 import switch2019.project.model.person.Person;
 import switch2019.project.model.category.Category;
