79be20a40fd1db6f8e6b989c4c790206186618ac
marta <1191780@isep.ipp.pt>
2020-03-10 15:33:35 +0000

    ref #612 fix some Denomination code smells: changed denomination field name

--- src/main/java/switch2019/project/model/shared/Denomination.java
+++ src/main/java/switch2019/project/model/shared/Denomination.java
@@ -6,29 +6,29 @@ import java.util.Objects;
 public class Denomination {
 
     //Private instance of denomination
-    private String denomination;
+    private String denominationValue;
 
     //Denomination constructor
-    public Denomination(String denomination) {
-        setValidDenomination(denomination);
+    public Denomination(String denominationValue) {
+        setValidDenomination(denominationValue);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Denomination)) return false;
-        Denomination denomination = (Denomination) o;
-        return this.denomination.equals(denomination.denomination);
+        Denomination denominationValue = (Denomination) o;
+        return this.denominationValue.equals(denominationValue.denominationValue);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(denomination);
+        return Objects.hash(denominationValue);
     }
 
     @Override
     public String toString() {
-        return denomination;
+        return denominationValue;
     }
 
     /**
@@ -44,7 +44,7 @@ public class Denomination {
 
         } else {
             newDenomination = removeWordAccents(removeSpecialCharacters(newDenomination).toUpperCase());
-            this.denomination = newDenomination;
+            this.denominationValue = newDenomination;
         }
     }
 
