66304bf5a0bf96178d307586546c6f2ef9f4fc86
Marta Pinheiro <1191778@isep.ipp.pt>
2020-03-10 09:57:33 +0000

    fix #616

---
+++ src/main/java/switch2019/project/model/person/PersonName.java
@@ -0,0 +1,69 @@
+package switch2019.project.model.person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PersonName {
+    /**
+     * Private Instance Variable
+     */
+
+    private String personName;
+
+    /**
+     * Constructor of PersonName
+     * @param personName
+     */
+
+    public PersonName(String personName) {
+        this.personName = setPersonName(personName);
+    }
+
+    /**
+     * Capitalize all the first letters and remove all the extra spaces
+     * @param personName
+     */
+
+    private static String setPersonName(String personName) {
+        personName = removeAllExtraSpaces(personName);
+        personName = capitalizeEachWords(personName);
+        return exceptionalCases(personName);
+    }
+
+    /**
+     * Remove all extra spaces of a string
+     * @param personName
+     */
+
+    private static String removeAllExtraSpaces(String personName) {
+        return personName.trim().replaceAll(" +", " ");
+    }
+
+    /**
+     * Capitalize all the individual words of a String
+     * @param personName
+     */
+
+    private static String capitalizeEachWords(String personName) {
+        personName = personName.toLowerCase();
+        personName = personName.substring(0, 1).toUpperCase() + personName.substring(1);
+        String[] words = personName.split(" ");
+        for(int i = 1 ; i < words.length ; i++){
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+        } return String.join(" ", words);
+    }
+
+    /**
+     * Exceptional cases so it doesn't capitalize "de", "da", "do", "das" and "dos" and capitalizes a single letter before a break
+     * @param personName
+     */
+
+    private static String exceptionalCases(String personName) {
+        ArrayList exceptions = new ArrayList<>(Arrays.asList("De", "Da", "Do", "Das", "Dos"));
+        String[] words = personName.split(" ");
+        for(int i = 0; i < words.length; i++) {
+            if(exceptions.contains(words[i]))
+                words[i] = words[i].toLowerCase();
+        } return String.join(" ", words);
+    }
+}
