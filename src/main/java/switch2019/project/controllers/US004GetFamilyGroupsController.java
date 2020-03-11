30df28cd2323711b679f98b33d0e0729f324987e
alex <1191743@isep.ipp.pt>
2020-03-11 15:13:56 +0000

    ref #643 updated SD for US004GetFamilyGroups and name of the classes

--- src/main/java/switch2019/project/controllers/US004GetFamilyGroupsController.java
+++ src/main/java/switch2019/project/controllers/US004GetFamilyGroupsController.java
@@ -5,7 +5,7 @@ import switch2019.project.model.person.Person;
 import switch2019.project.model.shared.Description;
 import switch2019.project.repository.GroupsRepository;
 
-public class US004GetFamilyGroups {
+public class US004GetFamilyGroupsController {
 
     //ALTERAR
     public boolean getFamilyGroups(Object service ,Description groupName, Person creator) {
