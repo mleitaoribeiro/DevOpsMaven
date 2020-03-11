94d59cad6f0ed90c01bcb59d031a7567224e0952
alex <1191743@isep.ipp.pt>
2020-03-11 11:06:37 +0000

    fix #638 created package for Controllers and US004 Class

---
+++ src/main/java/switch2019/project/controllers/US004IsGroupFamily.java
@@ -0,0 +1,15 @@
+package switch2019.project.controllers;
+
+import switch2019.project.model.group.Group;
+import switch2019.project.model.person.Person;
+import switch2019.project.model.shared.Description;
+import switch2019.project.repository.GroupsRepository;
+
+public class US004IsGroupFamily {
+
+    //ALTERAR
+    public boolean isGroupFamily(Object service ,Description groupName, Person creator) {
+        return false;
+    }
+
+}
