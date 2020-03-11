6dcbfd40e96a8afeca74f54105f04c8a3bebbecf
Marta Pinheiro <1191778@isep.ipp.pt>
2020-03-11 16:03:18 +0000

    fix #647

--- src/test/java/switch2019/project/model/ledger/ScheduleTest.java
+++ src/test/java/switch2019/project/model/ledger/ScheduleTest.java
@@ -1,22 +1,5 @@
 package switch2019.project.model.ledger;
 
-import org.junit.jupiter.api.Test;
-import switch2019.project.model.person.PersonName;
-import switch2019.project.model.shared.DateAndTime;
-import switch2019.project.model.shared.Denomination;
-import switch2019.project.model.shared.Description;
-import switch2019.project.model.shared.MonetaryValue;
-import switch2019.project.model.account.Account;
-import switch2019.project.model.person.Person;
-import switch2019.project.model.category.Category;
-import switch2019.project.model.person.Address;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Currency;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class ScheduleTest {
 
 }
