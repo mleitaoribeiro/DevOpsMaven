2bb4bd7cdbff5117e39e455a483112a72c0d7458
Marta Ribeiro <1191779@isep.ipp.pt>
2020-03-14 16:34:36 +0000

    ref #665

--- src/main/java/switch2019/project/model/frameworks/Owner.java
+++ src/main/java/switch2019/project/model/frameworks/Owner.java
@@ -4,9 +4,21 @@ import switch2019.project.model.person.Person;
 
 public interface Owner {
 
+    /**
+     * method to create a new Account
+     *
+     * @param accountDenomination
+     * @param accountDescription
+     */
     //este método vai ser alterado posteriormente quando existirem os IDS
     public boolean createAccount(String accountDenomination, String accountDescription);
 
+    /**
+     * method to create a new Category
+     *
+     * @param nameOfCategory
+     * @param categoryCreator
+     */
     //este método vai ser alterado posteriormente quando existirem os IDS
     public boolean createCategory(String nameOfCategory, Person categoryCreator);
 }
