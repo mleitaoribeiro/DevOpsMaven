e59131d1ddeb1ac63f05ecf5761c2acb71eb90c8
Kelle <1191782@isep.ipp.pt>
2020-03-14 18:58:59 +0000

    fixes #663 categoryID

--- src/main/java/switch2019/project/model/shared/CategoryID.java
+++ src/main/java/switch2019/project/model/shared/CategoryID.java
@@ -1,4 +1,50 @@
 package switch2019.project.model.shared;
 
-public class CategoryID {
+import switch2019.project.model.frameworks.ID;
+import switch2019.project.model.frameworks.Owner;
+
+import java.util.Objects;
+
+public class CategoryID implements ID {
+
+    private Denomination denomination;
+    private ID id;
+
+    /**
+     * CategoryID constructor
+     */
+    public CategoryID(Denomination denomination, Owner owner) {
+        this.denomination = denomination;
+        id = owner.getID();
+    }
+
+    /**
+     * Method to return Denomination
+     *
+     * @return denomination
+     */
+    public String getDenomination() {
+        return denomination.toString();
+    }
+
+    /**
+     * Override to Equals method
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryID that = (CategoryID) o;
+        return Objects.equals(denomination, that.denomination) &&
+                Objects.equals(id, that.id);
+    }
+
+    /**
+     * Override to hashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(denomination, id);
+    }
+
 }
---
+++ src/test/java/switch2019/project/model/shared/CategoryIDTest.java
@@ -0,0 +1,96 @@
+package switch2019.project.model.shared;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import switch2019.project.model.category.Category;
+import switch2019.project.model.group.Group;
+import switch2019.project.model.person.Address;
+import switch2019.project.model.person.Person;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryIDTest {
+    @Test
+    @DisplayName("Test if two CategoryID are the same - true case")
+    void testEqualCategoryIDTrueCase() {
+        //Arrange:
+        Person person1 = new Person("Raquel", new DateAndTime(1989, 1, 1),
+                new Address("Porto"), new Address("Rua xpto", "Porto", "4430-300"));
+        CategoryID categoryID1 = new CategoryID(new Denomination("Gym"), person1);
+        CategoryID categoryID2 = new CategoryID(new Denomination("Gym"), person1);
+
+        //Act:
+        boolean result = categoryID1.equals(categoryID2);
+
+        //Assert:
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Test if two CategoryID are the same - same object")
+    void testEqualsCategoryIDSameObject() {
+        //Arrange:
+        Person person1 = new Person("Raquel", new DateAndTime(1989, 1, 1),
+                new Address("Porto"), new Address("Rua xpto", "Porto", "4430-300"));
+        CategoryID categoryID1 = new CategoryID(new Denomination("Gym"), person1);
+        CategoryID categoryID2 = categoryID1;
+
+        //Act:
+        boolean result = categoryID1.equals(categoryID2);
+
+        //Assert:
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Test if two CategoryID are the same - false case - same owner, different denomination")
+    void testEqualsCategoryIDSameOwnerDifferentDenomination() {
+        //Arrange:
+        Person person1 = new Person("Raquel", new DateAndTime(1989, 1, 1),
+                new Address("Porto"), new Address("Rua xpto", "Porto", "4430-300"));
+        CategoryID categoryID1 = new CategoryID(new Denomination("Food"), person1);
+        CategoryID categoryID2 = new CategoryID(new Denomination("House"), person1);
+
+        //Act:
+        boolean result = categoryID1.equals(categoryID2);
+
+        //Assert:
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Test if two CategoryID are the same - false case - different owner, same denomination")
+    void testEqualsCategoryIDDifferentOwnerSameDenomination() {
+        //Arrange:
+        Person person1 = new Person("Raquel", new DateAndTime(1989, 1, 1),
+                new Address("Porto"), new Address("Rua xpto", "Porto", "4430-300"));
+        Group group1 = new Group("The Ones");
+        CategoryID categoryID1 = new CategoryID(new Denomination("House"), person1);
+        CategoryID categoryID2 = new CategoryID(new Denomination("House"), group1);
+
+        //Act:
+        boolean result = categoryID1.equals(categoryID2);
+
+        //Assert:
+        assertFalse(result);
+    }
+
+
+    @Test
+    @DisplayName("Test if two CategoryID are the same - different type of objects")
+    void testEqualsCategoryIDDifferentType() {
+        //Arrange:
+        Person person1 = new Person("Raquel", new DateAndTime(1989, 1, 1),
+                new Address("Porto"), new Address("Rua xpto", "Porto", "4430-300"));
+        Group group1 = new Group("The Ones");
+        CategoryID categoryID1 = new CategoryID(new Denomination("Gym"), person1);
+        Category category1 = new Category("Food");
+
+        //Act:
+        boolean result = categoryID1.equals(category1);
+
+        //Assert:
+        assertFalse(result);
+    }
+    
+}
