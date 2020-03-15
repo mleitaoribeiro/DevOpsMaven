0b3fb35c07a073874b0c0668a22c861c745a28ed
alex <1191743@isep.ipp.pt>
2020-03-15 14:59:33 +0000

    ref #671 controller created for US002.1

---
+++ src/main/java/switch2019/project/controllers/US002_1CreateGroupAndBecomeAdminController.java
@@ -0,0 +1,11 @@
+package switch2019.project.controllers;
+
+import switch2019.project.model.shared.PersonID;
+import switch2019.project.model.shared.Description;
+
+public class US002_1CreateGroupAndBecomeAdminController {
+
+    //public boolean class createGroupAndBecomeAdmin(Object service, Description groupDescription, PersonID personID) {
+     //  return service.createGroupAndBecomeAdmin(groupDescription, personID);
+    //}
+}
