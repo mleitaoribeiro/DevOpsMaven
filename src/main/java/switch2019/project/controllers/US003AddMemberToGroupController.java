f815245614cb1dfb574e33c2f726116d118217d1
Elsa <1191762@isep.ipp.pt>
2020-03-16 23:26:10 +0000

    fix #688

--- src/main/java/switch2019/project/controllers/US003AddMemberToGroupController.java
+++ src/main/java/switch2019/project/controllers/US003AddMemberToGroupController.java
@@ -6,13 +6,16 @@ import switch2019.project.model.shared.DateAndTime;
 import switch2019.project.model.shared.Description;
 import switch2019.project.model.shared.GroupID;
 import switch2019.project.model.shared.PersonID;
+import switch2019.project.repository.GroupsRepository;
+import switch2019.project.repository.PersonRepository;
+import switch2019.project.services.US003AddMemberToGroupService;
 
 import java.time.LocalDateTime;
 
 public class US003AddMemberToGroupController {
 
-        public boolean addMemberToGroup(Object service , PersonID personID,
-                                         GroupID groupID, LocalDateTime startingGroupDate) {
-        return false;
+    public boolean addMemberToGroup(GroupsRepository groupsRepository, PersonRepository personRepository, US003AddMemberToGroupService service, PersonID personID,
+                                    GroupID groupID) {
+        return service.addMemberToGroup(groupID, personID, groupsRepository, personRepository);
     }
 }
