1a197bd59a4b4fa6d0d1db0404810a70c4a2dfb9
alex <1191743@isep.ipp.pt>
2020-03-16 12:12:00 +0000

    fix #677 US002.1 updated

--- src/main/java/switch2019/project/controllers/US002_1CreateGroupAndBecomeAdminController.java
+++ src/main/java/switch2019/project/controllers/US002_1CreateGroupAndBecomeAdminController.java
@@ -2,10 +2,15 @@ package switch2019.project.controllers;
 
 import switch2019.project.model.shared.PersonID;
 import switch2019.project.model.shared.Description;
+import switch2019.project.services.US002_1createGroupAndBecomeAdminService;
 
-public class US002_1CreateGroupAndBecomeAdminController {
+public class US002_1CreateGroupAndBecomeAdminController{
 
-    //public boolean class createGroupAndBecomeAdmin(Object service, Description groupDescription, PersonID personID) {
-     //  return service.createGroupAndBecomeAdmin(groupDescription, personID);
-    //}
+    public boolean createGroupAndBecomeAdmin(US002_1createGroupAndBecomeAdminService service,
+                                             Description groupDescription, PersonID personID) {
+        return service.createGroupAndBecomeAdmin(groupDescription, personID);
+    }
+
+
 }
+
