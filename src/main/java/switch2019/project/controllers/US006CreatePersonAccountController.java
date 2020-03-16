660381ea89432069c386e644460c3b717b975c3a
marta <1191780@isep.ipp.pt>
2020-03-16 12:49:32 +0000

    ref #679 add method to controller

--- src/main/java/switch2019/project/controllers/US006CreatePersonAccountController.java
+++ src/main/java/switch2019/project/controllers/US006CreatePersonAccountController.java
@@ -1,4 +1,21 @@
 package switch2019.project.controllers;
 
+import switch2019.project.model.shared.Denomination;
+import switch2019.project.model.shared.Description;
+import switch2019.project.services.createPersonAccountService;
+
 public class US006CreatePersonAccountController {
+
+    /**
+     * Create Person Account
+     * @param service
+     * @param personName
+     * @param accountDenomination
+     * @param accountDescription
+     * @return
+     */
+
+    public boolean createPersonAccount(createPersonAccountService service, String personName, Denomination accountDenomination, Description accountDescription) {
+        return service.createPersonAccount(personName, accountDenomination, accountDescription);
+    }
 }
