3e9b2ca69f81849eca1b7e58a0455fd898e8af42
Elsa <1191762@isep.ipp.pt>
2020-03-16 23:14:35 +0000

    ref #689

--- src/main/java/switch2019/project/controllers/US003AddMemberToGroupController.java
+++ src/main/java/switch2019/project/controllers/US003AddMemberToGroupController.java
@@ -4,11 +4,15 @@ import switch2019.project.model.person.Address;
 import switch2019.project.model.person.Person;
 import switch2019.project.model.shared.DateAndTime;
 import switch2019.project.model.shared.Description;
+import switch2019.project.model.shared.GroupID;
+import switch2019.project.model.shared.PersonID;
+
+import java.time.LocalDateTime;
 
 public class US003AddMemberToGroupController {
 
-        public boolean addMemberToGroup(Object service , String memberName, DateAndTime memberBirthDate,
-                                        Address memberHomeAddress, Person memberMother, Person memberFather, Description groupDescription) {
+        public boolean addMemberToGroup(Object service , PersonID personID,
+                                         GroupID groupID, LocalDateTime startingGroupDate) {
         return false;
     }
 }
--- src/main/java/switch2019/project/services/US003AddMemberToGroupService.java
+++ src/main/java/switch2019/project/services/US003AddMemberToGroupService.java
@@ -2,13 +2,11 @@ package switch2019.project.services;
 
 import switch2019.project.model.group.Group;
 import switch2019.project.model.person.Person;
-import switch2019.project.model.shared.Description;
 import switch2019.project.model.shared.GroupID;
 import switch2019.project.model.shared.PersonID;
 import switch2019.project.repository.GroupsRepository;
 import switch2019.project.repository.PersonRepository;
 
-import java.time.LocalDateTime;
 
 public class US003AddMemberToGroupService {
 
