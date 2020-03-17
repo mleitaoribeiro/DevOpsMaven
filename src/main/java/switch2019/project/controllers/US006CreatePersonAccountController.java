6a5711ec01ab3f638528b8134d6742cdcd309974
marta <1191780@isep.ipp.pt>
2020-03-17 12:25:46 +0000

    ref #681 #679 Sprint 4 - US006 - changed controller and service parameters

--- src/main/java/switch2019/project/controllers/US006CreatePersonAccountController.java
+++ src/main/java/switch2019/project/controllers/US006CreatePersonAccountController.java
@@ -3,7 +3,10 @@ package switch2019.project.controllers;
 import switch2019.project.model.person.Email;
 import switch2019.project.model.shared.Denomination;
 import switch2019.project.model.shared.Description;
+import switch2019.project.repository.AccountRepository;
+import switch2019.project.repository.PersonRepository;
 import switch2019.project.services.US006CreatePersonAccountService;
+
 
 public class US006CreatePersonAccountController {
 
@@ -18,7 +21,12 @@ public class US006CreatePersonAccountController {
      * @return
      */
 
-    public boolean createPersonAccount(US006CreatePersonAccountService service, Email personEmail, Denomination accountDenomination, Description accountDescription) {
-        return service.createPersonAccount(personEmail, accountDenomination, accountDescription);
+    public boolean createPersonAccount(US006CreatePersonAccountService service, PersonRepository personRepository,
+                                       AccountRepository accountRepository, Email personEmail, Denomination accountDenomination,
+                                       Description accountDescription) {
+
+        return service.createPersonAccount(personRepository, accountRepository, personEmail, accountDenomination, accountDescription);
+
     }
 }
+
