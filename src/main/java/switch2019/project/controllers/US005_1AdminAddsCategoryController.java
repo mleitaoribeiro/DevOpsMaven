aa380da7c59b2bff44019abbc9e4055220dcef24
GabrielMoço(1191765) <1191765@isep.ipp.pt>
2020-03-17 13:10:30 +0000

    ref #674 Controller for the US 5.1 Created.

--- src/main/java/switch2019/project/controllers/US005_1AdminAddsCategoryController.java
+++ src/main/java/switch2019/project/controllers/US005_1AdminAddsCategoryController.java
@@ -1,4 +1,23 @@
 package switch2019.project.controllers;
 
+import switch2019.project.model.shared.GroupID;
+import switch2019.project.model.shared.PersonID;
+import switch2019.project.repository.CategoryRepository;
+import switch2019.project.repository.GroupsRepository;
+import switch2019.project.repository.PersonRepository;
+import switch2019.project.services.US005_1AdminAddsCategoryToCategoryListService;
+
+/**
+ * Call AdminAddsCategoryToCategoryListService to implement the User Story 5.1 using this controller.
+ *
+ */
+
 public class US005_1AdminAddsCategoryController {
+
+    public boolean addCategoryToGroupController(GroupID groupID, PersonID personID, CategoryRepository categoryRepository, String categoryDescription,
+                                      GroupsRepository groupsRepository, PersonRepository personRepository, US005_1AdminAddsCategoryToCategoryListService service) {
+
+        return service.addCategoryToGroup(groupID, personID, categoryRepository, categoryDescription, groupsRepository, personRepository);
+    }
+
 }
