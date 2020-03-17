add500a03b735b9051d0d347e9c72976daf67950
DiDias <1191755@isep.ipp.pt>
2020-03-17 14:41:18 +0000

    ref#698 Sprint 4 - DDD Refactoring - US004 Controller remake

--- src/main/java/switch2019/project/services/US004GetFamilyGroupsService.java
+++ src/main/java/switch2019/project/services/US004GetFamilyGroupsService.java
@@ -1,13 +1,8 @@
 package switch2019.project.services;
 
 import switch2019.project.model.group.Group;
-import switch2019.project.model.person.Person;
-import switch2019.project.model.shared.Description;
-import switch2019.project.model.shared.PersonID;
 import switch2019.project.repository.GroupsRepository;
-import switch2019.project.repository.PersonRepository;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 public class US004GetFamilyGroupsService {
