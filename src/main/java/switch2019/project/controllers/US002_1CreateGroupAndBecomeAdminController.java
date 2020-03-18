29ef3f840a3fd57035700df8ffc54c2e1afe0da2
alex <1191743@isep.ipp.pt>
2020-03-18 15:33:01 +0000

    fix #670 fix #671 added javadoc

--- src/main/java/switch2019/project/controllers/US002_1CreateGroupAndBecomeAdminController.java
+++ src/main/java/switch2019/project/controllers/US002_1CreateGroupAndBecomeAdminController.java
@@ -2,8 +2,6 @@ package switch2019.project.controllers;
 
 import switch2019.project.model.shared.PersonID;
 import switch2019.project.model.shared.Description;
-import switch2019.project.repository.GroupsRepository;
-import switch2019.project.repository.PersonRepository;
 import switch2019.project.services.US002_1CreateGroupAndBecomeAdminService;
 
 public class US002_1CreateGroupAndBecomeAdminController {
@@ -13,6 +11,15 @@ public class US002_1CreateGroupAndBecomeAdminController {
     public US002_1CreateGroupAndBecomeAdminController(US002_1CreateGroupAndBecomeAdminService service) {
         this.service = service;
     }
+
+    /**
+     * US002.1
+     * I want to create a group and become an Admin
+     *
+     * @param groupDescription
+     * @param personID
+     * @return true the group was created and person is now Admin
+     */
 
     public boolean createGroupAndBecomeAdmin(Description groupDescription, PersonID personID) {
         return service.createGroupAndBecomeAdmin(groupDescription, personID);
--- src/main/java/switch2019/project/services/US002_1CreateGroupAndBecomeAdminService.java
+++ src/main/java/switch2019/project/services/US002_1CreateGroupAndBecomeAdminService.java
@@ -1,6 +1,5 @@
 package switch2019.project.services;
 
-import switch2019.project.model.group.Group;
 import switch2019.project.model.person.Person;
 import switch2019.project.model.shared.Description;
 import switch2019.project.model.shared.PersonID;
@@ -12,10 +11,20 @@ public class US002_1CreateGroupAndBecomeAdminService {
     private GroupsRepository groupsRepository;
     private PersonRepository personRepository;
 
+
     public US002_1CreateGroupAndBecomeAdminService(GroupsRepository groupsRepository, PersonRepository personRepository) {
         this.groupsRepository = groupsRepository;
         this.personRepository = personRepository;
     }
+
+    /**
+     * US002.1
+     * I want to create a group and become an Admin
+     *
+     * @param groupDescription
+     * @param personID
+     * @return true the group was created and person is now Admin
+     */
 
     public boolean createGroupAndBecomeAdmin(Description groupDescription, PersonID personID) {
 
