7c0563aeb63273ba34eabca169794dd2112fd996
DiDias <1191755@isep.ipp.pt>
2020-03-18 15:43:16 +0000

    ref#698 Sprint 4 - DDD Refactoring - US004 Controller review

--- src/main/java/switch2019/project/controllers/US004GetFamilyGroupsController.java
+++ src/main/java/switch2019/project/controllers/US004GetFamilyGroupsController.java
@@ -1,13 +1,9 @@
 package switch2019.project.controllers;
 
 import switch2019.project.model.group.Group;
-import switch2019.project.model.person.Person;
-import switch2019.project.model.shared.Description;
 import switch2019.project.repository.GroupsRepository;
 import switch2019.project.services.US004GetFamilyGroupsService;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class US004GetFamilyGroupsController {
@@ -20,11 +16,10 @@ public class US004GetFamilyGroupsController {
 
     /**
      * US004 - As system manager I want to know which groups are families
-     * @param service
      * @param groupsRepository
      * @return
      */
-    public Set<Group> getFamilyGroups(US004GetFamilyGroupsService service, GroupsRepository groupsRepository) {
+    public Set<Group> getFamilyGroups(GroupsRepository groupsRepository) {
         return service.getFamilyGroups(groupsRepository) ;
     }
 
