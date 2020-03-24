014af3ecb52f5ad978a031b2487333b878cc9925
Marta Pinheiro <1191778@isep.ipp.pt>
2020-03-24 17:45:32 +0000

    fix #737 - added override for equals and hashcode + tests

--- src/test/java/switch2019/project/DTO/AdminCreateGroupDTOTest.java
+++ src/test/java/switch2019/project/DTO/AdminCreateGroupDTOTest.java
@@ -1,10 +1,15 @@
 package switch2019.project.DTO;
 
 import org.junit.jupiter.api.Test;
+import switch2019.project.domain.domainEntities.shared.Denomination;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminCreateGroupDTOTest {
+
+    /**
+     * Tests for the different getter methods
+     */
 
     @Test
     void getGroupDescription() {
@@ -32,4 +37,116 @@ class AdminCreateGroupDTOTest {
         //Assert
         assertEquals(expected,actual);
     }
+
+    /**
+     * Tests for the equals override
+     */
+
+    @Test
+    void equalsSameObject() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.equals(dto);
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    void equalsNullObject() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = null;
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    void equalsDifferentObjectClass() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        Denomination denomination = new Denomination("School");
+
+        //Act
+        boolean actual = dto.equals(denomination);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    void equalsSameAttributes() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    void equalsDifferentGroupDescription() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = new AdminCreateGroupDTO("School", "marta@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    void equalsDifferentPersonEmail() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = new AdminCreateGroupDTO("Games", "diana@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    /**
+     * Tests for the hashcode override
+     */
+
+    @Test
+    void sameHashcode() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.hashCode() == dto2.hashCode();
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    void differentHashcode() {
+        //Arrange
+        AdminCreateGroupDTO dto = new AdminCreateGroupDTO("Games", "marta@isep.ipp.pt");
+        AdminCreateGroupDTO dto2 = new AdminCreateGroupDTO("Games", "diana@isep.ipp.pt");
+
+        //Act
+        boolean actual = dto.hashCode() == dto2.hashCode();
+
+        //Assert
+        assertFalse(actual);
+    }
+
 }
