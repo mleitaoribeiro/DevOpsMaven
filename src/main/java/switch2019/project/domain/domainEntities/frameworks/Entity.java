24f175a19c7123b6519ad70c0ba061529ae9883d
João Cardoso <1110120@isep.ipp.pt>
2020-04-01 16:53:03 +0100

    ref #777: Added method getID to The Entity Interface

--- src/main/java/switch2019/project/domain/domainEntities/frameworks/Entity.java
+++ src/main/java/switch2019/project/domain/domainEntities/frameworks/Entity.java
@@ -1,4 +1,13 @@
 package switch2019.project.domain.domainEntities.frameworks;
 
 public interface Entity {
+
+
+    /**
+     * method to get the Object ID
+     * @return owner's ID
+     */
+
+    ID getID ();
+
 }
--- src/main/java/switch2019/project/domain/domainEntities/frameworks/ID.java
+++ src/main/java/switch2019/project/domain/domainEntities/frameworks/ID.java
@@ -1,4 +1,5 @@
 package switch2019.project.domain.domainEntities.frameworks;
 
 public interface ID {
+
 }
