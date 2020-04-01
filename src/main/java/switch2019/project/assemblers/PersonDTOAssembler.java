cfbf793149c8637fc2ed804cd0b13e8b7334794d
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-04-01 16:15:42 +0100

    fix #760 PersonDTOAssembler updated, new methods and tests

--- src/main/java/switch2019/project/assemblers/PersonDTOAssembler.java
+++ src/main/java/switch2019/project/assemblers/PersonDTOAssembler.java
@@ -1,5 +1,6 @@
 package switch2019.project.assemblers;
 
+import switch2019.project.DTO.AreSiblingsDTO;
 import switch2019.project.DTO.SiblingsDTO;
 
 public class PersonDTOAssembler {
@@ -10,9 +11,18 @@ public class PersonDTOAssembler {
      * Method to create a siblingDTO from two Strings of Person Email
      * @param emailPersonOne
      * @param emailPersonTwo
+     * @return AreSiblingsDTO
+     */
+    public static AreSiblingsDTO createAreSiblingsDTO(String emailPersonOne, String emailPersonTwo) {
+        return new AreSiblingsDTO(emailPersonOne, emailPersonTwo);
+    }
+
+    /**
+     * Method to create a siblingDTO from boolean from Service
+     * @param result
      * @return SiblingsDTO
      */
-    public static SiblingsDTO creatSiblingsDTOSiblingsDTO(String emailPersonOne, String emailPersonTwo) {
-        return new SiblingsDTO(emailPersonOne, emailPersonTwo);
+    public static SiblingsDTO createSiblingsDTO(boolean result) {
+        return new SiblingsDTO(result);
     }
 }
---
+++ src/test/java/switch2019/project/assemblers/PersonDTOAssemblerTest.java
@@ -0,0 +1,28 @@
+package switch2019.project.assemblers;
+
+import org.junit.jupiter.api.Test;
+import switch2019.project.DTO.AreSiblingsDTO;
+import switch2019.project.DTO.SiblingsDTO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDTOAssemblerTest {
+
+    @Test
+    void createAreSiblingsDTO() {
+        AreSiblingsDTO areSiblingsDTO = PersonDTOAssembler.createAreSiblingsDTO("email1", "email2");
+
+        AreSiblingsDTO areSiblingsDTO1 = new AreSiblingsDTO(areSiblingsDTO.getEmailPersonOne(), areSiblingsDTO.getEmailPersonTwo());
+
+        assertEquals(areSiblingsDTO, areSiblingsDTO1);
+    }
+
+    @Test
+    void createSiblingsDTO() {
+        SiblingsDTO siblingsDTO = PersonDTOAssembler.createSiblingsDTO(true);
+
+        SiblingsDTO siblingsDTO1 = new SiblingsDTO(true);
+
+        assertEquals(siblingsDTO, siblingsDTO1);
+    }
+}
