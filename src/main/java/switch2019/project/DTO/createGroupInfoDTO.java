86add3b7bf49544a3d9db2546472870d7c9fc0df
Marta Ribeiro <1191779@isep.ipp.pt>
2020-04-02 17:19:17 +0100

    fix #799

---
+++ src/main/java/switch2019/project/DTO/createGroupInfoDTO.java
@@ -0,0 +1,61 @@
+package switch2019.project.DTO;
+
+import java.util.Objects;
+
+public class createGroupInfoDTO {
+    private String groupDescription;
+    private String personEmail;
+
+    public createGroupInfoDTO() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        createGroupInfoDTO that = (createGroupInfoDTO) o;
+        return Objects.equals(groupDescription, that.groupDescription) &&
+                Objects.equals(personEmail, that.personEmail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupDescription, personEmail);
+    }
+
+    /**
+     * Set groupDescription
+     *
+     * @param groupDescription
+     */
+    public void setGroupDescription(String groupDescription) {
+        this.groupDescription = groupDescription;
+    }
+
+    /**
+     * get Group Description
+     *
+     * @return groupDescription
+     */
+    public String getGroupDescription() {
+        return groupDescription;
+    }
+
+    /**
+     * set personEmail
+     *
+     * @param personEmail
+     */
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    /**
+     * get Person Email
+     *
+     * @return personEmail
+     */
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+}
