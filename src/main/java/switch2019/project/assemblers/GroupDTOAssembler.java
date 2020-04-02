5aa3a77813e338b1d88693f90066c12f888df9e8
Marta Ribeiro <1191779@isep.ipp.pt>
2020-04-02 17:20:51 +0100

    fix #772

--- src/main/java/switch2019/project/assemblers/GroupDTOAssembler.java
+++ src/main/java/switch2019/project/assemblers/GroupDTOAssembler.java
@@ -1,12 +1,8 @@
 package switch2019.project.assemblers;
 
-import switch2019.project.DTO.AddMemberDTO;
-import switch2019.project.DTO.AddedMemberDTO;
-import switch2019.project.DTO.AdminCreateGroupDTO;
-import switch2019.project.DTO.GroupDTO;
+import switch2019.project.DTO.*;
 import switch2019.project.domain.domainEntities.group.Group;
 import switch2019.project.domain.domainEntities.person.Person;
-import switch2019.project.domain.domainEntities.shared.Description;
 import switch2019.project.domain.domainEntities.shared.GroupID;
 
 public class GroupDTOAssembler {
@@ -26,16 +22,28 @@ public class GroupDTOAssembler {
 
     /**
      * method to create a DTO for group creating from primitives parameters
+     *
      * @param groupDescription
      * @param personEmail
      * @return AdminCreateGroupDTO
      */
-    public static AdminCreateGroupDTO createAdminCreateGroupDTO(String groupDescription, String personEmail) {
-        return new AdminCreateGroupDTO(groupDescription, personEmail);
+    public static createGroupDTO creationOfGroupDTO(String groupDescription, String personEmail) {
+        return new createGroupDTO(groupDescription, personEmail);
+    }
+
+    /**
+     * method to create a DTO from createGroupControllerREST to service
+     *
+     * @param dto
+     * @return
+     */
+    public static createGroupDTO transformOfCreationOfGroupDTO(createGroupInfoDTO dto) {
+        return new createGroupDTO(dto.getGroupDescription(), dto.getPersonEmail());
     }
 
     /**
      * method to create a DTO to Add a Member from primitives parameters
+     *
      * @param personEmail
      * @param groupDescription
      * @return AddMemberDTO
