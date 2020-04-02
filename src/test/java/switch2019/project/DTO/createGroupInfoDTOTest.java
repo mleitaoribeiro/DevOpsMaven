30dc79de6fb68155a3013fc88373a433cef85385
Raquel Santos <1191782@isep.ipp.pt>
2020-04-02 18:28:19 +0100

    fixes #800 - createGroupInfoDTO Tests

---
+++ src/test/java/switch2019/project/DTO/createGroupInfoDTOTest.java
@@ -0,0 +1,196 @@
+package switch2019.project.DTO;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import switch2019.project.domain.domainEntities.shared.Denomination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class createGroupInfoDTOTest {
+
+
+    @Test
+    @DisplayName("Test to getPersonEmail")
+    void getPersonEmail() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setPersonEmail("raquel@isep.pt");
+        String expected = "raquel@isep.pt";
+
+        //Act
+        String real = dto.getPersonEmail();
+
+        //Assert
+        assertEquals(expected, real);
+    }
+
+
+    @Test
+    @DisplayName("Test to getGroupDescription")
+    void getGroupDescription() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        String expected = "Gym Buddies";
+
+        //Act
+        String real = dto.getGroupDescription();
+
+        //Assert
+        assertEquals(expected, real);
+    }
+
+
+    /**
+     * Tests for the equals method
+     */
+
+    @Test
+    @DisplayName("Test to equals - Same Object")
+    void equalsSameObject() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        //Act
+        boolean actual = dto.equals(dto);
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Test to equals - Null Object")
+    void equalsNullObject() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = null;
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Test to equals - Object from different class")
+    void equalsDifferentObjectClass() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        Denomination denomination = new Denomination("Gym");
+
+        //Act
+        boolean actual = dto.equals(denomination);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Test to equals - Same attributes")
+    void equalsSameAttributes() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = new createGroupInfoDTO();
+        dto2.setGroupDescription("Gym Buddies");
+        dto2.setPersonEmail("raquel@isep.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Test to equals - Different Group Description")
+    void equalsDifferentGroupDescription() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = new createGroupInfoDTO();
+        dto2.setGroupDescription("Gym Team");
+        dto2.setPersonEmail("raquel@isep.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Test to equals - Different Person Email")
+    void equalsDifferentPersonEmail() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = new createGroupInfoDTO();
+        dto2.setGroupDescription("Gym Buddies");
+        dto2.setPersonEmail("kelle@isep.pt");
+
+        //Act
+        boolean actual = dto.equals(dto2);
+
+        //Assert
+        assertFalse(actual);
+    }
+
+    /**
+     * Tests for the hashcode method
+     */
+
+    @Test
+    @DisplayName("Test to HashCode - Same Hashcode")
+    void sameHashcode() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = new createGroupInfoDTO();
+        dto2.setGroupDescription("Gym Buddies");
+        dto2.setPersonEmail("raquel@isep.pt");
+
+        //Act
+        boolean actual = dto.hashCode() == dto2.hashCode();
+
+        //Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Test to HashCode - Different Hashcode")
+    void differentHashcode() {
+        //Arrange
+        createGroupInfoDTO dto = new createGroupInfoDTO();
+        dto.setGroupDescription("Gym Buddies");
+        dto.setPersonEmail("raquel@isep.pt");
+
+        createGroupInfoDTO dto2 = new createGroupInfoDTO();
+        dto2.setGroupDescription("Gym Team");
+        dto2.setPersonEmail("raquel@isep.pt");
+
+        //Act
+        boolean actual = dto.hashCode() == dto2.hashCode();
+
+        //Assert
+        assertFalse(actual);
+    }
+
+}
