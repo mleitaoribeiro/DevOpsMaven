d9801a048c45a512b5a15d1f985ff8e00db708e9
Marta Ribeiro <1191779@isep.ipp.pt>
2020-04-06 18:11:10 +0100

    ref #807 and ref #808

---
+++ src/test/java/switch2019/project/springBoot/integration/US004GetFamilyGroupsControllerRestTest.java
@@ -0,0 +1,9 @@
+package switch2019.project.springBoot.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class US004GetFamilyGroupsControllerRestTest {
+
+
+
+}
---
+++ src/test/java/switch2019/project/springBoot/unit/US004GetFamilyGroupsControllerRestTest.java
@@ -0,0 +1,18 @@
+package switch2019.project.springBoot.unit;
+
+class US004GetFamilyGroupsControllerRestTest {
+
+    //Já há uma issue para estes testes - #808
+
+    //testes Marta
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //testes Diana
+
+}
