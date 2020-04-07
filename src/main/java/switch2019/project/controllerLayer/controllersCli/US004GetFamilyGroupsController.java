75761a462b671915ebf4ab6fd6c6c6e26dd8d271
Marta Cardoso <1191780@isep.ipp.pt>
2020-04-07 23:11:31 +0100

    fix #815 and fix #808 added unit test for controller REST related with US004

--- src/main/java/switch2019/project/controllerLayer/controllersCli/US004GetFamilyGroupsController.java
+++ src/main/java/switch2019/project/controllerLayer/controllersCli/US004GetFamilyGroupsController.java
@@ -1,9 +1,11 @@
 package switch2019.project.controllerLayer.controllersCli;
 
+import org.springframework.stereotype.Component;
 import switch2019.project.DTO.SerializationDTO.GroupDTO;
 import switch2019.project.applicationLayer.US004GetFamilyGroupsService;
 import java.util.Set;
 
+@Component
 public class US004GetFamilyGroupsController {
 
     private US004GetFamilyGroupsService service;
--- src/test/java/switch2019/project/springBoot/unit/TestConfiguration.java
+++ src/test/java/switch2019/project/springBoot/unit/TestConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import switch2019.project.applicationLayer.US003AddMemberToGroupService;
+import switch2019.project.applicationLayer.US004GetFamilyGroupsService;
 import switch2019.project.applicationLayer.US007CreateGroupAccountService;
 
 @Profile("test")
@@ -16,6 +17,10 @@ public class TestConfiguration {
     public US003AddMemberToGroupService service() {
         return Mockito.mock(US003AddMemberToGroupService.class);
     }
+
+    @Bean
+    @Primary
+    public US004GetFamilyGroupsService serviceUS004() { return Mockito.mock(US004GetFamilyGroupsService.class); }
 
     @Bean
     @Primary
