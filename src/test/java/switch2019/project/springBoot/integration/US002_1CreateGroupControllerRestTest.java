33da9b7001c0df4360c457b925d59515aa77dd1c
João Cardoso <1110120@isep.ipp.pt>
2020-04-08 02:42:57 +0100

    ref #806: Added more Integration Tests to CreateGroupControllerRest (US002_1)

--- src/test/java/switch2019/project/springBoot/integration/US002_1CreateGroupControllerRestTest.java
+++ src/test/java/switch2019/project/springBoot/integration/US002_1CreateGroupControllerRestTest.java
@@ -1,16 +1,17 @@
 package switch2019.project.springBoot.integration;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.util.NestedServletException;
 import switch2019.project.AbstractTest;
 import switch2019.project.DTO.DeserializationDTO.CreateGroupInfoDTO;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class US002_1CreateGroupControllerRestTest extends AbstractTest {
@@ -23,13 +24,13 @@ class US002_1CreateGroupControllerRestTest extends AbstractTest {
 
     @Test
     @DisplayName("Test if an existing person creates a Group and becomes Admin - Main Scenario")
-    void addCategoryToGroupServiceTestEqual() throws Exception {
+    void createGroupAndBecomeAdmin_HappyCase() throws Exception {
 
+        //Arrange
         String uri = "/createGroup";
 
         final String groupDescriptionStr = "Online Shopping";
         final String personEmail = "1110120@isep.ipp.pt";
-
 
         CreateGroupInfoDTO createGroupInfoDTO = new CreateGroupInfoDTO ();
         createGroupInfoDTO.setGroupDescription(groupDescriptionStr);
@@ -37,18 +38,84 @@ class US002_1CreateGroupControllerRestTest extends AbstractTest {
 
         String inputJson = super.mapToJson((createGroupInfoDTO));
 
+        String expected = "{\"groupDescription\":\"" +groupDescriptionStr.toUpperCase() +"\"}";
+
+       //Act
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(inputJson)).andReturn();
+                .content(inputJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(expected))
+                .andReturn();
 
         int status = mvcResult.getResponse().getStatus();
-        assertEquals(201, status);
-
         String result = mvcResult.getResponse().getContentAsString();
-        String expected = "{\"groupDescription\":\"" +groupDescriptionStr.toUpperCase() +"\"}";
-        assertEquals(expected, result);
+
+        //Assert
+        Assertions.assertAll(
+                () -> assertEquals(201, status),
+                () -> assertEquals(expected, result)
+        );
 
     }
+
+
+    @Test
+    @DisplayName("Test if an existing person creates a Group and becomes Admin - person doesn't exist")
+    void createGroupAndBecomeAdmin_personDoesNotExists() throws Exception {
+
+        //Arrange
+        String uri = "/createGroup";
+
+        final String groupDescriptionStr = "Expenses";
+        final String personEmail = "qwerty@gmail.com";
+
+        CreateGroupInfoDTO createGroupInfoDTO = new CreateGroupInfoDTO();
+        createGroupInfoDTO.setGroupDescription(groupDescriptionStr);
+        createGroupInfoDTO.setPersonEmail(personEmail);
+
+        String inputJson = super.mapToJson((createGroupInfoDTO));
+
+        //Act & Assert
+        assertThatThrownBy(() -> mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)))
+                .hasCause(new IllegalArgumentException("No person found with that email."))
+                .isExactlyInstanceOf(NestedServletException.class);
+
+    }
+
+
+    @Test
+    @DisplayName("Test if an existing person creates a Group and becomes Admin - group Already Exists")
+    void createGroupAndBecomeAdmin_groupAlreadyExists() throws Exception {
+
+        //Arrange
+        String uri = "/createGroup";
+
+        final String groupDescriptionStr = "SWitCH";
+        final String personEmail = "morty@gmail.com";
+
+        CreateGroupInfoDTO createGroupInfoDTO = new CreateGroupInfoDTO();
+        createGroupInfoDTO.setGroupDescription(groupDescriptionStr);
+        createGroupInfoDTO.setPersonEmail(personEmail);
+
+        String inputJson = super.mapToJson((createGroupInfoDTO));
+
+        //Act
+        Throwable thrown = catchThrowable(() -> {
+            mvc.perform(MockMvcRequestBuilders.post(uri)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(inputJson));
+        });
+
+        //Assert
+        assertThat(thrown)
+                .hasCause(new IllegalArgumentException("This Group Description already exists."))
+                .isExactlyInstanceOf(NestedServletException.class);
+    }
+
+
 
 
 
