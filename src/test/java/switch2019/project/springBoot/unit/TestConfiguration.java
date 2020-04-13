9b29ef3a0413cce686ed40acc957bb19323515d7
Elsa <1191762@isep.ipp.pt>
2020-04-13 18:10:32 +0100

    fix #831

--- src/test/java/switch2019/project/springBoot/unit/TestConfiguration.java
+++ src/test/java/switch2019/project/springBoot/unit/TestConfiguration.java
@@ -12,26 +12,46 @@ import switch2019.project.applicationLayer.*;
 public class TestConfiguration {
     @Bean
     @Primary
-    public US003AddMemberToGroupService serviceUS003 () {
+    public US001AreSiblingsService serviceUS001() {
+        return Mockito.mock(US001AreSiblingsService.class);
+    }
+
+    @Bean
+    @Primary
+    public US002_1CreateGroupService serviceUS002_1() {
+        return Mockito.mock(US002_1CreateGroupService.class);
+    }
+
+
+    @Bean
+    @Primary
+    public US003AddMemberToGroupService serviceUS003() {
         return Mockito.mock(US003AddMemberToGroupService.class);
     }
 
     @Bean
     @Primary
-    public US005_1AdminAddsCategoryToGroupService serviceUS005_1() { return Mockito.mock(US005_1AdminAddsCategoryToGroupService.class); }
+    public US004GetFamilyGroupsService serviceUS004() {
+        return Mockito.mock(US004GetFamilyGroupsService.class);
+    }
+
 
     @Bean
     @Primary
-    public US004GetFamilyGroupsService serviceUS004() { return Mockito.mock(US004GetFamilyGroupsService.class); }
+    public US005_1AdminAddsCategoryToGroupService serviceUS005_1() {
+        return Mockito.mock(US005_1AdminAddsCategoryToGroupService.class);
+    }
+
 
     @Bean
     @Primary
-    public US006CreatePersonAccountService serviceUS006 ()  { return Mockito.mock(US006CreatePersonAccountService.class);
+    public US006CreatePersonAccountService serviceUS006() {
+        return Mockito.mock(US006CreatePersonAccountService.class);
     }
 
     @Bean
     @Primary
-    public US007CreateGroupAccountService serviceUS007 ()  {
+    public US007CreateGroupAccountService serviceUS007() {
         return Mockito.mock(US007CreateGroupAccountService.class);
     }
 }
