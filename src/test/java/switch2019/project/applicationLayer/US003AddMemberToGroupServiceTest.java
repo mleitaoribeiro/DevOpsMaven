8c5cd32f8850b4604d7d5bc35b5b282196571538
Elsa <1191762@isep.ipp.pt>
2020-04-15 21:47:12 +0100

    fix #840

--- src/test/java/switch2019/project/applicationLayer/US003AddMemberToGroupServiceTest.java
+++ src/test/java/switch2019/project/applicationLayer/US003AddMemberToGroupServiceTest.java
@@ -1,47 +1,20 @@
 package switch2019.project.applicationLayer;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import switch2019.project.DTO.ServiceDTO.AddMemberDTO;
 import switch2019.project.DTO.SerializationDTO.AddedMemberDTO;
-import switch2019.project.domain.domainEntities.person.Address;
-import switch2019.project.domain.domainEntities.person.Email;
-import switch2019.project.domain.domainEntities.person.Person;
-import switch2019.project.domain.domainEntities.shared.DateAndTime;
-import switch2019.project.domain.domainEntities.shared.Description;
-import switch2019.project.infrastructure.repositories.GroupsRepository;
-import switch2019.project.infrastructure.repositories.PersonRepository;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class US003AddMemberToGroupServiceTest {
 
+    @Autowired
+    private US003AddMemberToGroupService service;
 
-    private static PersonRepository personRepository;
-    private static GroupsRepository groupsRepository;
-    private static US003AddMemberToGroupService service;
-
-    @BeforeAll
-    static void universeSetUp() {
-        personRepository = new PersonRepository();
-        groupsRepository = new GroupsRepository();
-        service = new US003AddMemberToGroupService(personRepository, groupsRepository);
-
-
-        //Add people to Repository
-        Person person = personRepository.createPerson("José Cardoso", new DateAndTime(1995, 1, 13), new Address("Miragaia"),
-                new Address("Rua das Flores", "Porto", "4000-189"), new Email("jose.cardoso@hotmail.com"));
-        Person person2 = personRepository.createPerson("José Cardoso", new DateAndTime(1995, 1, 13), new Address("Miragaia"),
-                new Address("Rua das Flores", "Porto", "4000-189"), new Email("jo.cardoso@hotmail.com"));
-        Person person3 = personRepository.createPerson("José", new DateAndTime(1995, 12, 13), new Address("Miragaia"),
-                new Address("Rua X", "Porto", "4520-266"), new Email("father@isep.ipp.pt"));
-
-        //Add groups to Repository
-
-        groupsRepository.createGroup(new Description("familia"), person);
-        groupsRepository.createGroup(new Description("canto"), person3);
-    }
 
     /**
      * Test to add member to group
@@ -51,10 +24,10 @@ public class US003AddMemberToGroupServiceTest {
     @DisplayName("Test if a member was added to group")
     void addMemberToGroup() {
         //Arrange
-        String personEmail = "jo.cardoso@hotmail.com";
-        String groupDescription = "familia";
+        String personEmail = "beatriz.azevedo@gmail.com";
+        String groupDescription = "Split Expenses";
         AddMemberDTO addMemberDTO = new AddMemberDTO(personEmail, groupDescription);
-        String expected = "jo.cardoso@hotmail.com was added to group familia";
+        String expected = "beatriz.azevedo@gmail.com was added to group split expenses";
 
         //Act
         AddedMemberDTO addedMemberDTO = service.addMemberToGroup(addMemberDTO);
@@ -64,13 +37,13 @@ public class US003AddMemberToGroupServiceTest {
     }
 
     @Test
-    @DisplayName("Test if a member was added to group-Same Person")
+    @DisplayName("Test if a member was added to group - Same Person")
     void addMemberToGroupAlreadyIn() {
         //Arrange
-        String personEmail = "jose.cardoso@hotmail.com";
-        String groupDescription = "familia";
+        String personEmail = "1110120@isep.ipp.pt";
+        String groupDescription = "Friends";
         AddMemberDTO addMemberDTO = new AddMemberDTO(personEmail, groupDescription);
-        String expected = "jose.cardoso@hotmail.com is already on group familia";
+        String expected = "1110120@isep.ipp.pt is already on group friends";
 
         //Act
         AddedMemberDTO addedMemberDTO = service.addMemberToGroup(addMemberDTO);
