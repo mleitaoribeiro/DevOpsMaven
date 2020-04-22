820ea0da29ac063989987402184430627ae62474
Marta Cardoso <1191780@isep.ipp.pt>
2020-04-22 21:45:06 +0100

    fix #861 added unit tests with mockito to US001 service

--- src/test/java/switch2019/project/applicationLayer/unit/US001AreSiblingsServiceUnitTest.java
+++ src/test/java/switch2019/project/applicationLayer/unit/US001AreSiblingsServiceUnitTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import switch2019.project.DTO.ServiceDTO.AreSiblingsDTO;
 import switch2019.project.applicationLayer.US001AreSiblingsService;
 import switch2019.project.assemblers.PersonDTOAssembler;
