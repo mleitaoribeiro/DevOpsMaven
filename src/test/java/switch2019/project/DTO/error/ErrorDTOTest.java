3da4a34a3db844d0e99fa98f37f553a52e2586ee
Elsa <1191762@isep.ipp.pt>
2020-04-27 22:29:42 +0100

    fix #888

---
+++ src/test/java/switch2019/project/DTO/error/ErrorDTOTest.java
@@ -0,0 +1,31 @@
+package switch2019.project.DTO.error;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorDTOTest {
+        @Test
+        void get() {
+
+            ErrorDTO errorDTO = new ErrorDTO(HttpStatus.UNPROCESSABLE_ENTITY, "This category already exists", "This category already exists");
+            HttpStatus statusExpected = HttpStatus.UNPROCESSABLE_ENTITY;
+            String messageExpected = "This category already exists";
+            String errorExpected = "This category already exists";
+
+            //Act
+            HttpStatus statusActual = errorDTO.getStatus();
+            String messageActual = errorDTO.getMessage();
+            String errorActual = errorDTO.getMessage();
+            //Assert
+
+            Assertions.assertAll(
+                    () -> assertEquals(statusExpected, statusActual),
+                    () -> assertEquals(messageExpected, messageActual),
+                    () -> assertEquals(errorExpected, errorActual)
+            );
+        }
+    }
+
