06cfedabda1c249e52ddc27047943d64df6ed6e0
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-04-28 18:13:21 +0100

    fix #894 add tests for PersonIdDto

---
+++ src/test/java/switch2019/project/DTO/serializationDTO/PersonIDDTOTest.java
@@ -0,0 +1,41 @@
+package switch2019.project.DTO.SerializationDTO;
+
+import org.junit.jupiter.api.Test;
+import switch2019.project.domain.domainEntities.person.Email;
+import switch2019.project.domain.domainEntities.shared.PersonID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonIDDTOTest {
+
+    @Test
+    void getPersonID() {
+        PersonIDDTO personIDDTO = new PersonIDDTO("email@email.com");
+
+        assertEquals("email@email.com", personIDDTO.getPersonID());
+    }
+
+    @Test
+    void testEquals() {
+        PersonIDDTO personIDDTO = new PersonIDDTO("email@email.com");
+        PersonIDDTO personIDDTOanother = new PersonIDDTO("email@email.com");
+        PersonIDDTO personIDDTOfalse = new PersonIDDTO("otheremail@email.com");
+        PersonID personID = new PersonID(new Email("email@mail.com"));
+
+        assertEquals(personIDDTO, personIDDTO);
+        assertEquals(personIDDTO, personIDDTOanother);
+        assertNotEquals(personIDDTO, personIDDTOfalse);
+        assertNotEquals(personIDDTO, personID);
+
+    }
+
+    @Test
+    void testHashCode() {
+        PersonIDDTO personIDDTO = new PersonIDDTO("email@email.com");
+        PersonIDDTO personIDDTOanother = new PersonIDDTO("email@email.com");
+        PersonIDDTO personIDDTOfalse = new PersonIDDTO("otheremail@email.com");
+
+        assertEquals(personIDDTO.hashCode(), personIDDTOanother.hashCode());
+        assertNotEquals(personIDDTO.hashCode(), personIDDTOfalse.hashCode());
+    }
+}
