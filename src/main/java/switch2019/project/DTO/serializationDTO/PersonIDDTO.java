f840363f209e14179be31dad2ebf7fdf4e52a626
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-04-29 12:42:37 +0100

    fix #894 update on personIdDto and correct tests

--- src/main/java/switch2019/project/DTO/serializationDTO/PersonIDDTO.java
+++ src/main/java/switch2019/project/DTO/serializationDTO/PersonIDDTO.java
@@ -1,6 +1,7 @@
 package switch2019.project.DTO.SerializationDTO;
 
 import org.springframework.hateoas.RepresentationModel;
+import switch2019.project.domain.domainEntities.shared.PersonID;
 
 import java.util.Objects;
 
@@ -8,8 +9,8 @@ public class PersonIDDTO extends RepresentationModel<PersonIDDTO>{
 
     private String personID;
 
-    public PersonIDDTO(String result) {
-        this.personID = result;
+    public PersonIDDTO(PersonID personID) {
+        this.personID = personID.getEmail();
     }
 
 
