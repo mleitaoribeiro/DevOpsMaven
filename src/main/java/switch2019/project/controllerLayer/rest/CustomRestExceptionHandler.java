ce27075e66732e023b4acb74d1b427f12ef385ab
João Cardoso <1110120@isep.ipp.pt>
2020-05-02 16:19:43 +0100

    ref #884: quickfix - Changed comments

--- src/main/java/switch2019/project/controllerLayer/rest/CustomRestExceptionHandler.java
+++ src/main/java/switch2019/project/controllerLayer/rest/CustomRestExceptionHandler.java
@@ -27,13 +27,16 @@ import java.util.List;
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
-     * Error handler for error status 400
+     *
+     * Handler for MethodArgumentNotValidException - occurs when validation on an argument annotated with @Valid fails
+     *
      * @param exception
      * @param headers
      * @param status
      * @param request
      * @return
      */
+
     @Override // status 400 - Errors Handling
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             final MethodArgumentNotValidException exception,
@@ -64,11 +67,70 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Exception handler for MethodArgumentTypeMismatchExceptions
+     *
+     * Handler for HttpMediaTypeNotSupportedException – occurs when the client sends a request with unsupported media type
+     *
+     * @param exception
+     * @param headers
+     * @param status
+     * @param request
+     * @return
+     */
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(exception.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        exception.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+
+        ErrorDTO apiError = new ErrorDTO(HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                exception.getLocalizedMessage(), builder.substring(0, builder.length() - 2));
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    /**
+     *
+     * Handler for HttpRequestMethodNotSupportedException - occurs when the client sends a request with an unsupported HTTP method
+     *
+     * @param exception
+     * @param headers
+     * @param status
+     * @param request
+     * @return
+     */
+
+    @Override //status 405 - Error Handling
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(exception.getMethod());
+        builder.append(
+                " method is not supported for this request. Supported methods are ");
+        exception.getSupportedHttpMethods().forEach(t -> builder.append(t).append(" "));
+
+        ErrorDTO apiError = new ErrorDTO(HttpStatus.METHOD_NOT_ALLOWED,
+                exception.getLocalizedMessage(), builder.toString());
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+
+    /**
+     *
+     * Handler for MethodArgumentTypeMismatchException - occurs when the method argument has not the expected type
+     *
      * @param exception
      * @param request
      * @return
      */
+
     @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
     public ResponseEntity<Object> handleMethodArgumentTypeMismatchException(
             final MethodArgumentTypeMismatchException exception,
@@ -85,11 +147,14 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Exception handler for IllegalArgumentExceptions
+     *
+     * Handler for IllegalArgumentExceptions - occurs when the client enters an argument that is not valid
+     *
      * @param exception
      * @param request
      * @return
      */
+
     @ExceptionHandler({ IllegalArgumentException.class })
     public ResponseEntity<Object> handleIllegalArgumentException(
             final IllegalArgumentException exception,
@@ -106,11 +171,14 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Exception handler for ResourceAlreadyExistsExceptions
+     *
+     * Handler for ResourceAlreadyExistsExceptions - occurs when the client tries to create a resource that already exists
+     *
      * @param exception
      * @param request
      * @return
      */
+
     @ExceptionHandler({ ResourceAlreadyExistsException.class })
     public ResponseEntity<Object> handleResourceAlreadyExistsException(
             final ResourceAlreadyExistsException exception,
@@ -127,11 +195,14 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Exception handler for ArgumentNotFoundExceptions
+     *
+     * Handler for ArgumentNotFoundException - occurs when the resource needed to perform the operation is not found
+     *
      * @param exception
      * @param request
      * @return
      */
+
     @ExceptionHandler({ ArgumentNotFoundException.class })
     public ResponseEntity<Object> handleArgumentNotFoundException(
             final ArgumentNotFoundException exception,
@@ -148,11 +219,14 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Exception handler for NoPermissionExceptions
+     *
+     * Handler for NoPermissionException - occurs when the client tries to do an operation for which he is not allowed
+     *
      * @param exception
      * @param request
      * @return
      */
+
     @ExceptionHandler({ NoPermissionException.class })
     public ResponseEntity<Object> handleNoPermissionException(
             final NoPermissionException exception,
@@ -170,72 +244,17 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      *
-     * Handle HttpMediaTypeNotSupportedException – occurs when the client sends a request with unsupported media type
-     *
-     * @param ex
-     * @param headers
-     * @param status
-     * @param request
-     * @return
-     */
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
-            HttpMediaTypeNotSupportedException ex,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(ex.getContentType());
-        builder.append(" media type is not supported. Supported media types are ");
-        ex.getSupportedMediaTypes().forEach(t -> builder.append(t + ", "));
-
-        ErrorDTO apiError = new ErrorDTO(HttpStatus.UNSUPPORTED_MEDIA_TYPE,
-                ex.getLocalizedMessage(), builder.substring(0, builder.length() - 2));
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
-
-    /**
-     *
-     * Handle HttpRequestMethodNotSupportedException - occurs when the client sends a request with an unsupported HTTP method
-     *
-     * @param ex
-     * @param headers
-     * @param status
-     * @param request
-     * @return
-     */
-
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException ex,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(ex.getMethod());
-        builder.append(
-                " method is not supported for this request. Supported methods are ");
-        ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
-
-        ErrorDTO apiError = new ErrorDTO(HttpStatus.METHOD_NOT_ALLOWED,
-                ex.getLocalizedMessage(), builder.toString());
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
-
-    /**
-     *
      * Deals with all other exceptions that don't have specific handlers
      *
-     * @param ex
+     * @param someException
      * @param request
      * @return
      */
 
     @ExceptionHandler({ Exception.class })
-    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+    public ResponseEntity<Object> handleAll(Exception someException, WebRequest request) {
         ErrorDTO apiError = new ErrorDTO(
-                HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred");
+                HttpStatus.INTERNAL_SERVER_ERROR, someException.getLocalizedMessage(), "error occurred");
         return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 
