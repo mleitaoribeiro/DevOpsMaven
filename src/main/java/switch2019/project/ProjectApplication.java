dbf00de04a4d116ba0765fc943549dddb48c749c
GabrielMoço(1191765) <1191765@isep.ipp.pt>
2020-05-04 14:31:51 +0100

    ref #884 - ProjectApplication modified for error 404 handling

--- src/main/java/switch2019/project/ProjectApplication.java
+++ src/main/java/switch2019/project/ProjectApplication.java
@@ -5,15 +5,25 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import switch2019.project.infrastructure.dataLoader.Bootstrapper;
 
 @SpringBootApplication(scanBasePackages = {"switch2019.project.controllerLayer",
         "switch2019.project.infrastructure", "switch2019.project.applicationLayer"})
-
+@EnableWebMvc
+@RestController
 public class ProjectApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ProjectApplication.class, args);
+
+        //Necessary for 404:
+        DispatcherServlet dispatcher = (DispatcherServlet)context.getBean("dispatcherServlet");
+        dispatcher.setThrowExceptionIfNoHandlerFound(true);
     }
 
     @Autowired
