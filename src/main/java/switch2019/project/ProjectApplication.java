a8b4dc33d79363df3d1d8634e0da49d88a7834a6
GabrielMoço(1191765) <1191765@isep.ipp.pt>
2020-05-05 16:13:49 +0100

    fix #884 - Content Negotiator added and WebConfig class created. This allows Springboot to only consider specified extensions.

--- src/main/java/switch2019/project/ProjectApplication.java
+++ src/main/java/switch2019/project/ProjectApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import switch2019.project.infrastructure.dataLoader.Bootstrapper;
 
+
 @SpringBootApplication(scanBasePackages = {"switch2019.project.controllerLayer",
-        "switch2019.project.infrastructure", "switch2019.project.applicationLayer"})
+        "switch2019.project.infrastructure", "switch2019.project.applicationLayer",
+        "switch2019.project.utils.projectApplicationConfiguration"})
 @EnableWebMvc
 public class ProjectApplication implements ApplicationRunner {
 
@@ -21,7 +21,7 @@ public class ProjectApplication implements ApplicationRunner {
         ApplicationContext context = SpringApplication.run(ProjectApplication.class, args);
 
         //Necessary for 404:
-        DispatcherServlet dispatcher = (DispatcherServlet)context.getBean("dispatcherServlet");
+        DispatcherServlet dispatcher = (DispatcherServlet) context.getBean("dispatcherServlet");
         dispatcher.setThrowExceptionIfNoHandlerFound(true);
     }
 
