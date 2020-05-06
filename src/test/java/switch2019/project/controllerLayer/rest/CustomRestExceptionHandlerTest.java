36eab7b976ff9975c4bbc2852af1fa2c86ec669c
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-05-06 20:43:11 +0100

    ref #884 created class test for ExceptionHandler

---
+++ src/test/java/switch2019/project/controllerLayer/rest/CustomRestExceptionHandlerTest.java
@@ -0,0 +1,104 @@
+package switch2019.project.controllerLayer.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.request.WebRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomRestExceptionHandlerTest {
+
+    /**
+     *
+     */
+
+    @Test
+    void handleMethodArgumentNotValid() {
+
+    }
+
+
+    /**
+     *
+     */
+
+    @Test
+    void handleHttpMessageNotReadable() {
+    }
+
+
+    /**
+     *
+     */
+
+    @Test
+    void handleNoHandlerFoundException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleHttpMediaTypeNotSupported() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleHttpRequestMethodNotSupported() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleMethodArgumentTypeMismatchException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleIllegalArgumentException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleResourceAlreadyExistsException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleArgumentNotFoundException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleNoPermissionException() {
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    void handleAll() {
+    }
+}
