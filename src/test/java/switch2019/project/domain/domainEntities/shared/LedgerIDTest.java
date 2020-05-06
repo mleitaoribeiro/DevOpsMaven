16983e78adc351aeecb3fe29304b7a14f8693ee8
Elsa <1191762@isep.ipp.pt>
2020-05-07 00:04:04 +0100

    ref #922

--- src/test/java/switch2019/project/domain/domainEntities/shared/LedgerIDTest.java
+++ src/test/java/switch2019/project/domain/domainEntities/shared/LedgerIDTest.java
@@ -29,6 +29,61 @@ class LedgerIDTest {
     }
 
     @Test
+    @DisplayName("Test if two ledger ID are equals-False case")
+    void testEqualsLedgerIDFalse() {
+        //Arrange
+        Person person = new Person("Marta", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        Person person2 = new Person("Ana", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        LedgerID ledgerID = new LedgerID(person.getID());
+        LedgerID ledgerID2 = new LedgerID(person2.getID());
+
+        //Act
+        boolean result = ledgerID.equals(ledgerID2);
+
+        //Assert
+        assertTrue(result);
+
+    }
+
+    @Test
+    void testEqualsNull() {
+        //Arrange:
+        Person person = new Person("Marta", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        LedgerID ledgerID = new LedgerID(person.getID());
+        LedgerID ledgerID2 = null;
+
+        //Act
+        boolean result = ledgerID.equals(ledgerID2);
+
+        //Assert
+        assertFalse(result);
+
+    }
+
+    @Test
+    void testEqualsDifferentClass() {
+        //Arrange:
+        Person person = new Person("Marta", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        LedgerID ledgerID = new LedgerID(person.getID());
+        PersonID personID = new PersonID(new Email("1234@isep.pt"));
+
+        //Act
+        boolean result = ledgerID.equals(personID);
+
+        //Assert
+        assertFalse(result);
+
+    }
+
+    @Test
     void testHashCodeTrue() {
         //Arrange
         Person person = new Person("Marta", new DateAndTime(1996, 4, 27),
@@ -45,6 +100,24 @@ class LedgerIDTest {
     }
 
     @Test
+    void testHashCodeFalse() {
+
+        //Arrange
+        Person person = new Person("Marta", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        Person person2 = new Person("Ana", new DateAndTime(1996, 4, 27),
+                new Address("Porto"), new Address("Rua X", "Porto", "4450-365")
+                , new Email("1234@isep.pt"));
+        LedgerID ledgerID = new LedgerID(person.getID());
+        LedgerID ledgerID2 = new LedgerID(person2.getID());
+
+        //Act e Assert
+        assertEquals(ledgerID.hashCode(), ledgerID2.hashCode());
+    }
+
+    @Test
+    @DisplayName("Get Owner ID from ledgerID - PersonID")
     void getOwnerID() {
 
         //Arrange
