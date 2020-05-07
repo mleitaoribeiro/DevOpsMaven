ca95ad35cf0f85ce0f680cc261b1a68468f8e3f8
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-05-07 11:12:47 +0100

    ref #464 update ledger sorting method

--- src/main/java/switch2019/project/domain/domainEntities/ledger/Ledger.java
+++ src/main/java/switch2019/project/domain/domainEntities/ledger/Ledger.java
@@ -136,7 +136,6 @@ public class Ledger {
      */
 
     public void sortLedgerByTransactionDateAscending() {
-        sortLedgerByTransactionDateDescending();
         Collections.reverse(ledgerTransactions);
     }
 
