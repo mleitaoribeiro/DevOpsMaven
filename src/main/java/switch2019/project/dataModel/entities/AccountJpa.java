8dc229101da355cd229ea26f7a291215702bf7a4
Marta Pinheiro <1191778@isep.ipp.pt>
2020-05-13 15:32:51 +0100

    ref #934 added equals and hashcode to AccountJpa

--- src/main/java/switch2019/project/dataModel/entities/AccountJpa.java
+++ src/main/java/switch2019/project/dataModel/entities/AccountJpa.java
@@ -3,22 +3,32 @@ import javax.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Data
 @ToString
 @Entity(name ="accounts")
 public class AccountJpa {
 
     @EmbeddedId
-    private AccountKeyJpa accountKeyJpa;
+    private AccountIDJpa accountIDJpa;
     private String description;
     private String balance;
 
     protected AccountJpa() {};
 
     public AccountJpa(String owner, String denomination, String description, String balance) {
-        accountKeyJpa = new AccountKeyJpa(owner, denomination);
+        accountIDJpa = new AccountIDJpa(owner, denomination);
         this.description = description;
         this.balance = balance;
+    }
+
+    public AccountIDJpa getAccountIDJpa() {
+        return accountIDJpa;
+    }
+
+    public void setAccountIDJpa(AccountIDJpa accountIDJpa) {
+        this.accountIDJpa = accountIDJpa;
     }
 
     public String getDescription() {
@@ -37,5 +47,20 @@ public class AccountJpa {
         this.balance = balance;
     }
 
-    public AccountKeyJpa getAccountKeyJpa() { return accountKeyJpa; }
+    public AccountIDJpa getAccountKeyJpa() { return accountIDJpa; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AccountJpa)) return false;
+        AccountJpa that = (AccountJpa) o;
+        return Objects.equals(accountIDJpa, that.accountIDJpa) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(balance, that.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountIDJpa, description, balance);
+    }
 }
