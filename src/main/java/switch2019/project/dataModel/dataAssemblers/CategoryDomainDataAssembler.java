3a692f9a2aa67963d4c74f43ec32c15a2585b15d
João Cardoso <1110120@isep.ipp.pt>
2020-05-13 10:58:07 +0100

    fixed #933: added tests

--- src/main/java/switch2019/project/dataModel/dataAssemblers/CategoryDomainDataAssembler.java
+++ src/main/java/switch2019/project/dataModel/dataAssemblers/CategoryDomainDataAssembler.java
@@ -3,7 +3,6 @@ package switch2019.project.dataModel.dataAssemblers;
 
 import switch2019.project.dataModel.entities.CategoryJpa;
 import switch2019.project.domain.domainEntities.category.Category;
-import switch2019.project.domain.domainEntities.frameworks.Owner;
 import switch2019.project.domain.domainEntities.frameworks.OwnerID;
 import switch2019.project.domain.domainEntities.person.Email;
 import switch2019.project.domain.domainEntities.shared.Denomination;
@@ -15,11 +14,29 @@ import java.util.regex.Pattern;
 
 public class CategoryDomainDataAssembler {
 
-    public CategoryJpa toData (Category category) {
+
+    /**
+     *
+     * Method for transforming an Category object into a CategoryJPA object
+     *
+     * @param category
+     * @return categoryJPA
+     */
+
+    public static CategoryJpa toData (Category category) {
         return new CategoryJpa(category.getID().getOwnerIDString(), category.getNameOfCategory());
     }
 
-    public Category toDomain (CategoryJpa categoryJPA) {
+
+    /**
+     *
+     * Method for transforming an CategoryJPA object into a Category object
+     *
+     * @param categoryJPA
+     * @return category
+     */
+
+    public static Category toDomain (CategoryJpa categoryJPA) {
 
         Denomination categoryDenomination = new Denomination(categoryJPA.getCategoryKeyJPA().getDenomination());
 
@@ -35,7 +52,15 @@ public class CategoryDomainDataAssembler {
         return new Category (categoryDenomination, ownerID);
     }
 
-    private boolean isEmail(String owner) {
+    /**
+     *
+     * Auxiliary method to check if a string is an email (Person ID)
+     *
+     * @param owner
+     * @return
+     */
+
+    private static boolean isEmail(String owner) {
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
                 "[a-zA-Z0-9_+&*-]+)*@" +
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
