a54c38271d5305f904e73f6866fe3797e89f5510
Marta Pinheiro <1191778@isep.ipp.pt>
2020-05-13 15:34:57 +0100

    ref #934 added method findAllByAccountIDJpa_Owner to AccountJpaRepository

--- src/main/java/switch2019/project/infrastructure/jpa/AccountJpaRepository.java
+++ src/main/java/switch2019/project/infrastructure/jpa/AccountJpaRepository.java
@@ -3,11 +3,11 @@ package switch2019.project.infrastructure.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import switch2019.project.dataModel.entities.AccountJpa;
-import switch2019.project.domain.domainEntities.shared.AccountID;
 
 import java.util.List;
 
 @Repository
-public interface AccountJpaRepository extends JpaRepository<AccountJpa, AccountID> {
+public interface AccountJpaRepository extends JpaRepository<AccountJpa, String> {
     List<AccountJpa> findAll();
+    List<AccountJpa> findAllByAccountIDJpa_Owner(String accountIDJpa_owner);
 }
