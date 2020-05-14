9e67e14e463804994f764d39aa9920715ae0fd70
Marta Cardoso <1191780@isep.ipp.pt>
2020-05-14 22:14:54 +0100

    fix #924 added tests do AdressJPA

--- src/test/java/switch2019/project/dataModel/entities/AddressJpaTest.java
+++ src/test/java/switch2019/project/dataModel/entities/AddressJpaTest.java
@@ -1,40 +1,54 @@
 package switch2019.project.dataModel.entities;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressJpaTest {
 
-    @Test
-    void getId() {
+    private AddressJpa adressjpa;
+
+    @BeforeEach
+    public void setup() {
+        adressjpa = new AddressJpa("Rua das Almas", "Coimbra", "4601-501");
     }
 
     @Test
     void getStreet() {
+        //Arrange
+        String expected = "Rua das Almas";
+
+        //Act
+        String result = adressjpa.getStreet();
+
+        //Assert
+        assertEquals(expected, result);
     }
 
     @Test
     void getCity() {
+        //Arrange
+        String expected = "Coimbra";
+
+        //Act
+        String result = adressjpa.getCity();
+
+        //Assert
+        assertEquals(expected, result);
+
     }
 
     @Test
     void getPostalCode() {
-    }
+        //Arrange
+        String expected = "4601-501";
 
-    @Test
-    void setId() {
-    }
+        //Act
+        String result = adressjpa.getPostalCode();
 
-    @Test
-    void setStreet() {
-    }
-
-    @Test
-    void setCity() {
-    }
-
-    @Test
-    void setPostalCode() {
+        //Assert
+        assertEquals(expected, result);
     }
 }
