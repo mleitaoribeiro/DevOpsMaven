8924d9584df07af3f4835f240f0cd45fcd7858b0
Marta Pinheiro <1191778@isep.ipp.pt>
2020-05-14 22:20:08 +0100

    ref #936 deleted forgotten System.out.print

--- src/main/java/switch2019/project/dataModel/dataAssemblers/PersonDomainDataAssembler.java
+++ src/main/java/switch2019/project/dataModel/dataAssemblers/PersonDomainDataAssembler.java
@@ -14,23 +14,22 @@ public class PersonDomainDataAssembler {
 
     private PersonDomainDataAssembler() {};
 
-    public static PersonJpa toData(Person person ) {
-        return new PersonJpa( person.getID().toString(), person.getName(), person.getBirthDate(),
-                person.getBirthPlace().getBirthPlace(), new AddressJpa(person.getAddress().getStreet(), person.getAddress().getCity(),
-                person.getAddress().getPostalCode()));
+    public static PersonJpa toData(Person person) {
+        return new PersonJpa(person.getID().toString(), person.getName(), person.getBirthDate(),
+                person.getBirthPlace().getBirthPlace(), new AddressJpa(person.getAddress().getStreet(),
+                person.getAddress().getCity(), person.getAddress().getPostalCode()));
     }
 
-    public static Person toDomain( PersonJpa personJpa ) {
+    public static Person toDomain(PersonJpa personJpa) {
 
         String birthDateJpa = personJpa.getBirthDate();
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         //convert String to LocalDate
         LocalDate birthPlace = LocalDate.parse(birthDateJpa, formatter);
 
-        return new Person(personJpa.getName(), new DateAndTime (birthPlace.getYear(), birthPlace.getMonthValue(),
+        return new Person(personJpa.getName(), new DateAndTime(birthPlace.getYear(), birthPlace.getMonthValue(),
                 birthPlace.getDayOfMonth()), new Address(personJpa.getBirthPlace()),
-                new Address(personJpa.getAddress().getStreet(),personJpa.getAddress().getCity(), personJpa.getAddress().getPostalCode()),
-                new Email(personJpa.getEmail()));
+                new Address(personJpa.getAddress().getStreet(), personJpa.getAddress().getCity(),
+                        personJpa.getAddress().getPostalCode()), new Email(personJpa.getEmail()));
     }
 }
