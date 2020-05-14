2058737b30486512c6d1db999ceefd8943b57c8f
Elsa <1191762@isep.ipp.pt>
2020-05-14 18:43:23 +0100

    ref #936 add constructor

--- src/main/java/switch2019/project/domain/domainEntities/group/Group.java
+++ src/main/java/switch2019/project/domain/domainEntities/group/Group.java
@@ -3,6 +3,7 @@ package switch2019.project.domain.domainEntities.group;
 import switch2019.project.domain.domainEntities.shared.*;
 import switch2019.project.domain.domainEntities.frameworks.Owner;
 import switch2019.project.domain.domainEntities.person.Person;
+
 import java.util.*;
 
 public class Group implements Owner {
@@ -19,7 +20,7 @@ public class Group implements Owner {
         members = new HashSet<>();
         admins = new HashSet<>();
         this.addMember(groupCreator);
-        this.groupCreator=groupCreator;
+        this.groupCreator = groupCreator;
     }
 
     public Group(Description description, PersonID groupCreator, DateAndTime dateAndTime) {
@@ -28,7 +29,14 @@ public class Group implements Owner {
         members = new HashSet<>();
         admins = new HashSet<>();
         this.addMember(groupCreator);
-        this.groupCreator=groupCreator;
+        this.groupCreator = groupCreator;
+    }
+
+    //Construtor with 2 list:Admins and members
+    public Group(Set<PersonID> members, Set<PersonID> admins, DateAndTime startingDate) {
+        this.members = members;
+        this.admins = admins;
+        this.startingDate = startingDate;
     }
 
     @Override
@@ -152,20 +160,22 @@ public class Group implements Owner {
 
     /**
      * Get the admins of one group
+     *
      * @return
      */
     public Set<PersonID> getAdmins() {
-       Set<PersonID> auxAdmins =  new HashSet<>();
+        Set<PersonID> auxAdmins = new HashSet<>();
         auxAdmins.addAll(this.admins);
         return auxAdmins;
     }
 
     /**
      * Get members of one group
+     *
      * @return
      */
     public Set<PersonID> getMembers() {
-        Set<PersonID> auxMembers =  new HashSet<>();
+        Set<PersonID> auxMembers = new HashSet<>();
         auxMembers.addAll(this.members);
         return auxMembers;
     }
@@ -183,7 +193,7 @@ public class Group implements Owner {
         return false;
     }
 
-    public String getStartingDate(){
+    public String getStartingDate() {
         return this.startingDate.yearMonthDayToString();
     }
 
