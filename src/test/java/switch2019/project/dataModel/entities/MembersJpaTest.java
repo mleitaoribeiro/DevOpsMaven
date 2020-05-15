cb24b1746ff87e4bd7624f1a60be55716ccee4ed
Elsa <1191762@isep.ipp.pt>
2020-05-15 09:38:55 +0100

    ref #952 kill add Tests

--- src/test/java/switch2019/project/dataModel/entities/MembersJpaTest.java
+++ src/test/java/switch2019/project/dataModel/entities/MembersJpaTest.java
@@ -48,6 +48,58 @@ class MembersJpaTest {
         assertFalse(result);
     }
 
+    @Test
+    void testEqualsDifferentGroup() {
+
+        GroupJpa groupJpa = new GroupJpa("", "", "");
+
+        GroupJpa groupJpa1 = new GroupJpa(" ", "s", "s");
+
+        MembersJpa member1 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        MembersJpa member3 = new MembersJpa(groupJpa1, "1191755@isep.ipp.pt");
+
+        assertNotEquals(member1, member3);
+
+    }
+
+    @Test
+    void testEqualsSame() {
+
+        GroupJpa groupJpa = new GroupJpa("", "", "");
+
+        MembersJpa member1 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        MembersJpa member3 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        assertEquals(member1, member3);
+
+    }
+
+    @Test
+    void testEqualsMembers() {
+
+
+        MembersJpa member1 = new MembersJpa();
+
+        assertEquals(member1, member1);
+
+    }
+
+    @Test
+    void testEqualsById() {
+
+        GroupJpa groupJpa = new GroupJpa("", "", "");
+
+        MembersJpa.MembersIdJpa memmberid = new MembersJpa.MembersIdJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        MembersJpa member3 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+
+        assertEquals(member3.getId(), memmberid);
+
+    }
+
 
     @Test
     void testHashCode() {
@@ -57,6 +109,21 @@ class MembersJpaTest {
         MembersJpa member2 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
 
         assertEquals(member1.hashCode(), member2.hashCode());
+    }
+
+    @Test
+    void testHasCodeNotEqualsGroup() {
+
+        GroupJpa groupJpa = new GroupJpa("", "", "");
+
+        GroupJpa groupJpa1 = new GroupJpa(" ", "s", "s");
+
+        MembersJpa member1 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        MembersJpa member2 = new MembersJpa(groupJpa1, "1191755@isep.ipp.pt");
+
+
+        assertNotEquals(member1.hashCode(), member2.hashCode());
     }
 
     @Test
@@ -73,5 +140,24 @@ class MembersJpaTest {
 
         //Assert
         assertEquals(expected, result);
+    }
+
+    @Test
+    void testToStringNotEquals() {
+
+        //Arrange
+        GroupJpa groupJpa = new GroupJpa("SWITCH", "1191762@isep.ipp.pt", "2002-09-10");
+
+        MembersJpa member1 = new MembersJpa(groupJpa, "1191755@isep.ipp.pt");
+
+        MembersJpa member2 = new MembersJpa(groupJpa, "119175@isep.ipp.pt");
+
+        //Act
+        String resultOne = member1.toString();
+
+        String resultTwo = member2.toString();
+
+        //Assert
+        assertNotEquals(resultOne, resultTwo);
     }
 }
