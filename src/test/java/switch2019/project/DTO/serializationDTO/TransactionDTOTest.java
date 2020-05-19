b4a92c0f50f91b098c704464bb661b8720c9b01d
João Cardoso <1110120@isep.ipp.pt>
2020-05-19 18:18:14 +0100

    ref #958: Added tests to TransactionDTO - tests to HashCode & more tests to equals

--- src/test/java/switch2019/project/DTO/serializationDTO/TransactionDTOTest.java
+++ src/test/java/switch2019/project/DTO/serializationDTO/TransactionDTOTest.java
@@ -2,13 +2,18 @@ package switch2019.project.DTO.serializationDTO;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import switch2019.project.domain.domainEntities.category.Category;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionDTOTest {
 
 
-
+    /**
+     *
+     * Tests to Equals
+     *
+     */
 
     @Test
     @DisplayName("Test if two transactions are the equals - True - Exactly same object")
@@ -28,7 +33,7 @@ class TransactionDTOTest {
 
     @Test
     @DisplayName("Test if two transactions are the equals - True - Different equals objects")
-    void testEquals() {
+    void testEqualsSameObject() {
 
         //Arrange
         TransactionDTO transactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
@@ -179,8 +184,88 @@ class TransactionDTOTest {
 
 
     @Test
-    void testHashCode() {
+    @DisplayName("Test if two transactions are the equals - False- Different Objects")
+    void testEqualsDifferentObjects() {
+
+        //Arrange
+        TransactionDTO transactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "CREDIT");
+
+       CategoryDTO otherTransactionDTO = new CategoryDTO("REVOLUT", "1110120@isep.ipp.pt");
+
+        //Act
+        boolean isEquals = transactionDTO.equals(otherTransactionDTO);
+
+        //Assert
+        assertFalse(isEquals);
     }
+
+
+    @Test
+    @DisplayName("Test if two transactions are the equals - False - Null")
+    void testEqualsNull() {
+
+        //Arrange
+        TransactionDTO transactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "CREDIT");
+
+        TransactionDTO otherTransactionDTO = null;
+
+
+        //Act
+        boolean isEquals = transactionDTO.equals(otherTransactionDTO);
+
+        //Assert
+        assertFalse(isEquals);
+    }
+
+
+    /**
+     *
+     * Tests to HashCode
+     *
+     */
+
+    @Test
+    @DisplayName("Tests to HashCode - True")
+    void testSameHashCode() {
+
+        //Arrange
+        TransactionDTO transactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "CREDIT");
+
+        TransactionDTO otherTransactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "CREDIT");
+
+        //Act
+        int transactionDTOHashcode = transactionDTO.hashCode();
+        int otherTransactionDTOHashcode = otherTransactionDTO.hashCode();
+
+        //Assert
+        assertEquals(transactionDTOHashcode, otherTransactionDTOHashcode);
+    }
+
+    @Test
+    @DisplayName("Tests to HashCode - True")
+    void testDifferentHashCode() {
+
+        //Arrange
+        TransactionDTO transactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "CREDIT");
+
+        TransactionDTO otherTransactionDTO = new TransactionDTO("2 EUR", "PAYMENT", "20-3-2020",
+                "ONLINE SHOPPING", "REVOLUT", "ALIEXPRESS", "DEBIT");
+
+        //Act
+        int transactionDTOHashcode = transactionDTO.hashCode();
+        int otherTransactionDTOHashcode = otherTransactionDTO.hashCode();
+
+        //Assert
+        assertNotEquals(transactionDTOHashcode, otherTransactionDTOHashcode);
+
+    }
+
+
 
     @Test
     void getAmount() {
