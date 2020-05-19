a93c7cf7df3bba4a6a6b6c592134d991dba49b18
Alexandre Oliveira <1191743@isep.ipp.pt>
2020-05-19 17:52:50 +0100

    fix #962 added transformToCreateGroupTransactionDTO method and tests

--- src/main/java/switch2019/project/assemblers/LedgerDTOAssembler.java
+++ src/main/java/switch2019/project/assemblers/LedgerDTOAssembler.java
@@ -1,6 +1,8 @@
 package switch2019.project.assemblers;
 
+import switch2019.project.DTO.deserializationDTO.CreateTransactionInfoDTO;
 import switch2019.project.DTO.serializationDTO.TransactionDTO;
+import switch2019.project.DTO.serviceDTO.CreateGroupTransactionDTO;
 import switch2019.project.domain.domainEntities.ledger.Transaction;
 
 public class LedgerDTOAssembler {
@@ -15,6 +17,15 @@ public class LedgerDTOAssembler {
     /**
      * This method transforms a CreateGroupTransactionInfoDTO into a CreateGroupTransactionDTO
      */
+
+    public static CreateGroupTransactionDTO transformToCreateGroupTransactionDTO(String groupDescription,
+                                                CreateTransactionInfoDTO createTransactionInfoDTO){
+
+        return new CreateGroupTransactionDTO(groupDescription, createTransactionInfoDTO.getAmount(),
+                createTransactionInfoDTO.getCurrency(), createTransactionInfoDTO.getDescription(),
+                createTransactionInfoDTO.getCategory(), createTransactionInfoDTO.getAccountFrom(),
+                createTransactionInfoDTO.getAccountTo(), createTransactionInfoDTO.getType());
+    }
 
     /**
      * Method to create the TransactionDTO from Domain Object
