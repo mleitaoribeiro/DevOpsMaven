9ce7d500aa92cc979d39fd054b718e3dab06d2ca
Marta Ribeiro <1191779@isep.ipp.pt>
2020-05-19 16:35:17 +0100

    fix #962

---
+++ src/main/java/switch2019/project/assemblers/LedgerDTOAssembler.java
@@ -0,0 +1,24 @@
+package switch2019.project.assemblers;
+
+public class LedgerDTOAssembler {
+
+    public LedgerDTOAssembler() {
+    }
+
+    /**
+     * This method transforms a CreatePersonalTransactionInfoDTO into a CreatePersonalTransactionDTO
+     */
+
+    /**
+     * This method transforms a CreateGroupTransactionInfoDTO into a CreateGroupTransactionDTO
+     */
+
+    /**
+     * Method to create the TransactionDTO from Domain Object
+     */
+
+    /**
+     * Method to create the TransactionShortDTO from Domain Object
+     */
+
+}
