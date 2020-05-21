749b84b3e7d89dc5d35867257972827af0ab07fb
Elsa <1191762@isep.ipp.pt>
2020-05-21 16:43:48 +0100

    fix #987

---
+++ src/main/java/switch2019/project/infrastructure/jpa/TransactionJpaRepository.java
@@ -0,0 +1,14 @@
+package switch2019.project.infrastructure.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import switch2019.project.dataModel.entities.TransactionJpa;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionJpaRepository extends JpaRepository<TransactionJpa, Long> {
+    List<TransactionJpa> findAll();
+    Optional<TransactionJpa> findById(Long id);
+
+
+}
