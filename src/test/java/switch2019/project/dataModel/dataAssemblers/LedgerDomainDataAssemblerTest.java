16b671ad3f2540e25ee76103f484b3874093f938
Marta Pinheiro <1191778@isep.ipp.pt>
2020-05-22 11:25:00 +0100

    ref #986 - fixed test date

--- src/test/java/switch2019/project/dataModel/dataAssemblers/LedgerDomainDataAssemblerTest.java
+++ src/test/java/switch2019/project/dataModel/dataAssemblers/LedgerDomainDataAssemblerTest.java
@@ -5,6 +5,7 @@ import switch2019.project.dataModel.entities.LedgerIdJpa;
 import switch2019.project.dataModel.entities.LedgerJpa;
 import switch2019.project.domain.domainEntities.ledger.Ledger;
 import switch2019.project.domain.domainEntities.person.Email;
+import switch2019.project.domain.domainEntities.shared.DateAndTime;
 import switch2019.project.domain.domainEntities.shared.PersonID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,7 +45,7 @@ class LedgerDomainDataAssemblerTest {
 
         // Arrange
         Ledger ledger = new Ledger(new PersonID(new Email("marta@gmail.com")));
-        LedgerJpa ledgerJpa = new LedgerJpa(new LedgerIdJpa("marta@gmail.com"), "2020-05-22");
+        LedgerJpa ledgerJpa = new LedgerJpa(new LedgerIdJpa("marta@gmail.com"), new DateAndTime().yearMonthDayToString());
 
         // Act
         LedgerJpa ledgerFromDomain = LedgerDomainDataAssembler.toData(ledger);
